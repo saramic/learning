54d53b18583ec4eeb9ec33534ca25dcc3f8ca0ad
Michael Milewski <michael.milewski@redbubble.com>
2017-02-18 12:59:42 +1100

    roman hardcoded cases

--- dojo/ruby_roman_in_order/spec/roman_spec.rb
+++ dojo/ruby_roman_in_order/spec/roman_spec.rb
@@ -18,4 +18,12 @@ describe Roman do
   it_should_behave_like 'roman', 8, 'VIII'
   it_should_behave_like 'roman', 9, 'IX'
   it_should_behave_like 'roman', 10, 'X'
+  it_should_behave_like 'roman', 40, 'XL'
+  it_should_behave_like 'roman', 50, 'L'
+  it_should_behave_like 'roman', 90, 'XC'
+  it_should_behave_like 'roman', 100, 'C'
+  it_should_behave_like 'roman', 400, 'CD'
+  it_should_behave_like 'roman', 500, 'D'
+  it_should_behave_like 'roman', 900, 'CM'
+  it_should_behave_like 'roman', 1000, 'M'
 end
