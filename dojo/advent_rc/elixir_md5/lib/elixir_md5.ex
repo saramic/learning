9e9d318dd94f599c79ea0f9073dfe639e3f9d680
Michael Milewski <michael.milewski@redbubble.com>
2017-02-28 14:48:12 +1100

    start of the md5 implementation in elixir as a mix project

--- dojo/advent_rc/elixir_md5/lib/elixir_md5.ex
+++ dojo/advent_rc/elixir_md5/lib/elixir_md5.ex
@@ -1,18 +1,14 @@
 defmodule ElixirMd5 do
-  @moduledoc """
-  Documentation for ElixirMd5.
-  """
+  def md5(number) do
+    :crypto.hash(:md5, "abbhdwsy#{number}")
+      |> Base.encode16(case: :lower)
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirMd5.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def do_md5 do
+    Enum.join Stream.iterate(0, &(&1+1))
+      |> Stream.map(fn(a) -> md5(a) end)
+      |> Stream.filter(fn(a) -> Regex.run(~r{^00000}, a) != nil end)
+      |> Enum.take(8)
+      |> Enum.map(fn(a) -> Enum.slice(to_charlist(a), 5, 1) end)
   end
 end
--- dojo/advent_rc/elixir_md5/test/elixir_md5_test.exs
+++ dojo/advent_rc/elixir_md5/test/elixir_md5_test.exs
@@ -2,7 +2,13 @@ defmodule ElixirMd5Test do
   use ExUnit.Case
   doctest ElixirMd5
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "md5 number with key" do
+    assert ElixirMd5.md5(1739529) == "000008bfb72caf77542c32b53a73439b"
+  end
+
+  # TODO slow - could fake out the Stream.iterate to only return
+  # [1739529, 1910966, 1997199, 2854555, 2963716, 3237361, 4063427, 7777889]
+  test "do_md5" do
+    assert ElixirMd5.do_md5 == "801b56a7x"
   end
 end
