850a6b4c45cd65bf4eedc34e032527781510f95d
Michael Milewski <michael.milewski@redbubble.com>
2017-04-08 14:36:26 +1000

    add legend

--- react/d3ia/src/BarChart.js
+++ react/d3ia/src/BarChart.js
@@ -3,6 +3,8 @@ import './App.css';
 import { scaleLinear } from 'd3-scale';
 import { max, sum } from 'd3-array';
 import { select } from 'd3-selection';
+import { legendColor } from 'd3-svg-legend';
+import { transition } from 'd3-transition';
 
 class BarChart extends Component {
   constructor(props) {
@@ -22,24 +24,42 @@ class BarChart extends Component {
     const node = this.node;
     const dataMax = max(this.props.data.map(d => sum(d.data)));
     const barWidth = this.props.size[0] / this.props.data.length;
+
+    const legend = legendColor()
+      .scale(this.props.colorScale)
+      .labels(["Wave 1", "Wave 2", "Wave 3", "Wave 4"]);
+
+    select(node)
+      .selectAll("g.legend")
+      .data([0])
+      .enter()
+      .append("g")
+      .attr("class", "legend")
+      .call(legend);
+
+    select(node)
+      .select("g.legend")
+      .attr("transform", "translate(" + (this.props.size[0] - 130) + ", 20)");
+
     const yScale = scaleLinear()
       .domain([0, dataMax])
       .range([0, this.props.size[1]]);
 
     select(node)
-      .selectAll('rect')
+      .selectAll('rect.bar')
       .data(this.props.data)
       .enter()
-      .append('rect');
+      .append('rect')
+      .attr("class", "bar");
 
     select(node)
-      .selectAll('rect')
+      .selectAll('rect.bar')
       .data(this.props.data)
       .exit()
       .remove();
 
     select(node)
-      .selectAll('rect')
+      .selectAll('rect.bar')
       .data(this.props.data)
       .attr('x', (d,i) => i * barWidth)
       .attr('y', d => this.props.size[1] - yScale(sum(d.data)))
