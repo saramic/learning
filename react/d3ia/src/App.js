805a5650d38f5f4fec1d3349c4342b762305e44d
Michael Milewski <michael.milewski@redbubble.com>
2017-04-08 12:33:26 +1000

    a basic bar chart as per d3ia ch 9.3

--- react/d3ia/src/App.js
+++ react/d3ia/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BarChart from './BarChart';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>d3ia dashboard</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <BarChart data={[5,10,1,3]} size={[500,500]} />
+        </div>
       </div>
     );
   }
