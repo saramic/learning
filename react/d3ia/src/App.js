a9f2bbad1d919bf47c21ae1f22ba281e64b38d86
Michael Milewski <michael.milewski@redbubble.com>
2017-04-08 13:44:15 +1000

    somewhat responsive

--- react/d3ia/src/App.js
+++ react/d3ia/src/App.js
@@ -17,6 +17,22 @@ appdata.forEach((d,i) => {
 });
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onResize = this.onResize.bind(this);
+    this.state = { screenWidth: 1000, screenHeight: 500 };
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize, false);
+    this.onResize();
+  }
+
+  onResize() {
+    this.setState({ screenWidth: window.innerWidth,
+      screenHeight: window.innerHeight - 70 });
+  }
+
   render() {
     const colorScale = scaleThreshold().domain([5,10,20,30,50])
       .range(["#75739F", "#5EAFC6", "#41A368", "#93C464", "#FE9922"]);
@@ -26,9 +42,18 @@ class App extends Component {
           <h2>d3ia dashboard</h2>
         </div>
         <div>
-          <StreamGraph colorScale={colorScale} data={appdata} size={[1000,250]} />
-          <WorldMap colorScale={colorScale} data={appdata} size={[500,400]} />
-          <BarChart colorScale={colorScale} data={appdata} size={[500,500]} />
+          <StreamGraph
+            colorScale={colorScale}
+            data={appdata}
+            size={[this.state.screenWidth, this.state.screenHeight /2]} />
+          <WorldMap
+            colorScale={colorScale}
+            data={appdata}
+            size={[this.state.screenWidth / 2, this.state.screenHeight /2]} />
+          <BarChart
+            colorScale={colorScale}
+            data={appdata}
+            size={[this.state.screenWidth / 2, this.state.screenHeight /2]} />
         </div>
       </div>
     );
