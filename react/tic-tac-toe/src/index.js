6eb3fc3770ab26086a075960c9f2bbde4a58c955
Michael Milewski <michael.milewski@redbubble.com>
2017-05-20 09:14:22 +1000

    step 2 - component shows the data from the state and a user interaction

--- react/tic-tac-toe/src/index.js
+++ react/tic-tac-toe/src/index.js
@@ -11,10 +11,17 @@ const times = x => f => {
 }
 
 class Square extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})} >
+        {this.state.value}
       </button>
     );
   }
