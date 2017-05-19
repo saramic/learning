3cccf66ca73dc3cd4c371d2b68359c040f6f2557
Michael Milewski <michael.milewski@redbubble.com>
2017-05-20 09:19:32 +1000

    step 3 - pull state up from Square to Board and click handling

--- react/tic-tac-toe/src/index.js
+++ react/tic-tac-toe/src/index.js
@@ -11,25 +11,34 @@ const times = x => f => {
 }
 
 class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})} >
-        {this.state.value}
+      <button className="square" onClick={this.props.onClick}>
+        {this.props.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
