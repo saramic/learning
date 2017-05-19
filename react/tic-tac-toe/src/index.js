5d549dd79bdef166dba945c723670fbdee6af8aa
Michael Milewski <michael.milewski@redbubble.com>
2017-05-20 09:25:56 +1000

    step 5 - players take turns X and O

--- react/tic-tac-toe/src/index.js
+++ react/tic-tac-toe/src/index.js
@@ -21,13 +21,17 @@ class Board extends React.Component {
     super();
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -38,7 +42,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
 
     return (
       <div>
