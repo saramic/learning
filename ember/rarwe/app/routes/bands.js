8847ce536a4c2e284c381f0de0fe08605214d9bf
Michael Milewski <saramic@gmail.com>
2017-09-10 13:05:41 +1000

    split songs and bands by routes

--- ember/rarwe/app/routes/bands.js
+++ ember/rarwe/app/routes/bands.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
+var Band = Ember.Object.extend({
+  name: '',
+});
+
+var ledZeppelin = Band.create({ name: 'Led Zeppelin' });
+var pearlJam = Band.create({ name: 'Pearl Jam' });
+var fooFighters = Band.create({ name: 'Foo Fighters' });
+
+var bands = [ledZeppelin, pearlJam, fooFighters];
+
 export default Ember.Route.extend({
+  model: function() {
+    return bands;
+  }
 });
--- ember/rarwe/app/routes/songs.js
+++ ember/rarwe/app/routes/songs.js
@@ -1,4 +1,51 @@
 import Ember from 'ember';
 
+var Song = Ember.Object.extend({
+  title: '',
+  rating: 0,
+  band: ''
+});
+
+var blackDog = Song.create({
+  title: 'Black Dog',
+  band: 'Led Zepplin',
+  rating: 3
+});
+
+var yellowLedbetter = Song.create({
+  title: 'Yellow Ledbetter',
+  band: 'Pearl Jam',
+  rating: 4
+});
+
+var pretender = Song.create({
+  title: 'The Pretender',
+  band: 'Foo FIghters',
+  rating: 2
+});
+
+var SongCollection = Ember.Object.extend({
+  content: [],
+  sortProperties: ['rating:desc'],
+  sortedContent: Ember.computed.sort('content', 'sortProperties'),
+});
+
+var songs = SongCollection.create();
+songs.get('content').pushObject(blackDog);
+songs.get('content').pushObject(yellowLedbetter);
+songs.get('content').pushObject(pretender);
+
+var alwaysWaiting = Song.create({
+  title: 'Always Waiting',
+  band: 'Kaya Project',
+  rating: 5
+})
+
+window.newSong = alwaysWaiting;
+window.songs = songs;
+
 export default Ember.Route.extend({
+  model: function() {
+    return songs;
+  }
 });
