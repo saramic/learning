682658b448c12a1c38d141745eaf4d4f8817ac42
Michael Milewski <saramic@gmail.com>
2017-09-27 23:48:16 +1000

    added a quick rest service in rust for JSON time and hello world

--- lang/rust/rust_in_action/ch1-time-api/src/main.rs
+++ lang/rust/rust_in_action/ch1-time-api/src/main.rs
@@ -1,3 +1,36 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate serde;
+extern crate chrono;
+extern crate rocket;
+extern crate rocket_contrib;
+
+#[macro_use]
+extern crate serde_derive;
+
+use chrono::prelude::*;
+use rocket_contrib::{Json};
+
+#[derive(Serialize)]
+struct Timestamp {
+  time: String,
+}
+
+#[get("/")]
+fn index() -> &'static str {
+  "Hello, world!"
+}
+
+#[get("/time")]
+fn time_now() -> Json<Timestamp> {
+  let now: DateTime<Utc> = Utc::now();
+  let timestamp = Timestamp { time: now.to_rfc3339() };
+  Json(timestamp)
+}
+
 fn main() {
-    println!("Hello, world!");
+  rocket::ignite()
+  .mount("/", routes![index, time_now])
+  .launch();
 }
