3b11cc6a14b68bb9c9749583779e6ca770da5646
Michael Milewski <saramic@gmail.com>
2017-10-13 06:24:38 +1100

    simple addition to the hworld in rust

--- lang/rust/rust_in_action/hello/src/main.rs
+++ lang/rust/rust_in_action/hello/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+fn greet_world() {
     println!("Hello, world!");
+
+    let southern_germany = "Grüß Gott!";
+    let poland = "dzień dobry";
+    let japan = "ハロー・ワールド";
+
+    let regions = [southern_germany, poland, japan];
+
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+}
+
+fn main() {
+    greet_world();
 }
