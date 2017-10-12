ccfc46952058227de536f186908d6c0ff74d16be
Michael Milewski <saramic@gmail.com>
2017-10-13 06:32:56 +1100

    some number types

--- lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
+++ lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
+    let twenty = 20;
+    let twenty_one: i32 = twenty + 1;
+    let floats_okay = 21.0;
+    let one_million = 1_000_000;
+    
+    println!("{}; {}; {}; {}", twenty, twenty_one, floats_okay, one_million);
+
     let a = 10;
     let b: i32 = 20;
 
