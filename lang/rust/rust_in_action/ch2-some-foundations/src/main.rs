3a121879f87c3f106e8808f60f5036afa6a49178
Michael Milewski <saramic@gmail.com>
2017-10-13 06:39:34 +1100

    printing numbers in different bases

--- lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
+++ lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
@@ -1,10 +1,21 @@
 fn main() {
+    let three = 0b11;
+    let thirty = 0o36;
+    let three_hundred = 0x12C;
+
+    println!("{} {} {}", three, thirty, three_hundred);
+    println!("{:b} {:b} {:b}", three, thirty, three_hundred);
+    println!("{:o} {:o} {:o}", three, thirty, three_hundred);
+    println!("{:x} {:x} {:x}", three, thirty, three_hundred);
+    println!("");
+
     let twenty = 20;
     let twenty_one: i32 = twenty + 1;
     let floats_okay = 21.0;
     let one_million = 1_000_000;
     
     println!("{}; {}; {}; {}", twenty, twenty_one, floats_okay, one_million);
+    println!("");
 
     let a = 10;
     let b: i32 = 20;
