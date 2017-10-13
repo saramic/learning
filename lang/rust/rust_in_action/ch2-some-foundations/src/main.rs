2bc8fa5294539527b8bdee4343137bcbe9764aad
Michael Milewski <saramic@gmail.com>
2017-10-14 09:26:25 +1100

    mutable variable to track line number

--- lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
+++ lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
@@ -5,13 +5,15 @@ fn main() {
     let quote = "Every face, every shop, bedroom window, public-house, and
     dark square is a picture feverishly turned--in search of what?
     It is the same with books. What do we seek through millions of pages?";
+    let mut line_num: usize = 1;
 
     for line in quote.lines() {
         if line.contains(search_term) {
-            println!("{}", line);
+            println!("{}: {}", line_num, line);
         }
+        line_num += 1;
     }
-    println!("{}");
+    println!("");
 
     let (a, b) = (1.2, 3.4);
     let (x, y) = (10, 20);
