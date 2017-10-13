6f715d266a23d11b425ac38a809b3c24423a31ad
Michael Milewski <saramic@gmail.com>
2017-10-14 09:24:29 +1100

    a version of grep-lite

--- lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
+++ lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
@@ -1,6 +1,18 @@
 use std::ops::{Add};
 
 fn main() {
+    let search_term = "picture";
+    let quote = "Every face, every shop, bedroom window, public-house, and
+    dark square is a picture feverishly turned--in search of what?
+    It is the same with books. What do we seek through millions of pages?";
+
+    for line in quote.lines() {
+        if line.contains(search_term) {
+            println!("{}", line);
+        }
+    }
+    println!("{}");
+
     let (a, b) = (1.2, 3.4);
     let (x, y) = (10, 20);
 
