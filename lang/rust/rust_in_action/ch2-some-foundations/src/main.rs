e935ac48d097e4a5b5cd060a8dd3a1d1b7e4dc92
Michael Milewski <saramic@gmail.com>
2017-10-14 09:14:26 +1100

    simplification with &needle reference

--- lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
+++ lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
@@ -3,9 +3,8 @@ fn main() {
     let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
 
     for reference in haystack.iter() {
-        let item = *reference;
-        if item == needle {
-            println!("{}", item);
+        if reference == &needle {
+            println!("{}", reference)
         }
     }
     println!("");
