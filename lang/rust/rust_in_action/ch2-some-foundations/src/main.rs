d99c524d9ff447dd9226674c3bd75b012c8c58ac
Michael Milewski <saramic@gmail.com>
2017-10-14 09:17:05 +1100

    using match

--- lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
+++ lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
@@ -1,10 +1,16 @@
 fn main() {
-    let needle = 42;
     let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
 
     for reference in haystack.iter() {
-        if reference == &needle {
-            println!("{}", reference)
+        let item = *reference;
+
+        let result = match item {
+            42 | 132 => "hit!",
+            _ => "miss",
+        };
+
+        if result == "hit!" {
+            println!("{}: {}", item, result);
         }
     }
     println!("");
