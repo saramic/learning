f0bf5411fa4f3a70580988581998e1447163ea99
Michael Milewski <saramic@gmail.com>
2017-10-14 09:37:52 +1100

    arrays

--- lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
+++ lang/rust/rust_in_action/ch2-some-foundations/src/main.rs
@@ -1,6 +1,32 @@
 use std::ops::{Add};
 
 fn main() {
+    println!();
+
+    // ARRAYS
+    let one             = [1,2,3];
+    let two: [u8; 3]    = [1,2,3];
+    let blank1          = [0; 3];
+    let blank2: [u8; 3] = [0; 3];
+
+    let arrays = [one, two, blank1, blank2];
+
+    for a in &arrays {
+        print!("{:?}: ", a);
+        for n in a.iter() {
+            print!("\t{} + 10 = {}", n, n+10);
+        }
+
+        let mut sum = 0;
+        for i in 0..a.len() {
+            sum += a[i];
+        }
+        print!("\t(Σ{:?} = {})", a, sum);
+        println!();
+    }
+    println!();
+
+    // GREP-LITE
     let search_term = "picture";
     let quote = "Every face, every shop, bedroom window, public-house, and
     dark square is a picture feverishly turned--in search of what?
@@ -12,8 +38,9 @@ fn main() {
             println!("{}: {}", line_num, line);
         }
     }
-    println!("");
+    println!();
 
+    // NUMBERS AND GENERIC FUNCTION
     let (a, b) = (1.2, 3.4);
     let (x, y) = (10, 20);
 
@@ -22,8 +49,9 @@ fn main() {
 
     println!("{} + {} = {}", a, b, c);
     println!("{} + {} = {}", x, y, z);
-    println!("");
+    println!();
 
+    // SIMPLE FINDER
     let haystack = [1, 1, 2, 5, 14, 42, 132, 429, 1430, 4862];
 
     for reference in haystack.iter() {
@@ -38,8 +66,9 @@ fn main() {
             println!("{}: {}", item, result);
         }
     }
-    println!("");
+    println!();
 
+    // NUMBERS AND BASE SYSTEMS
     let three = 0b11;
     let thirty = 0o36;
     let three_hundred = 0x12C;
@@ -48,15 +77,16 @@ fn main() {
     println!("{:b} {:b} {:b}", three, thirty, three_hundred);
     println!("{:o} {:o} {:o}", three, thirty, three_hundred);
     println!("{:x} {:x} {:x}", three, thirty, three_hundred);
-    println!("");
+    println!();
 
+    // NUMBERS AND TYPES
     let twenty = 20;
     let twenty_one: i32 = twenty + 1;
     let floats_okay = 21.0;
     let one_million = 1_000_000;
     
     println!("{}; {}; {}; {}", twenty, twenty_one, floats_okay, one_million);
-    println!("");
+    println!();
 
     let a = 10;
     let b: i32 = 20;
@@ -65,6 +95,7 @@ fn main() {
     println!("a + b = {}", c);
 }
 
+//GENERIC FUNCTION
 fn add<T: Add<Output = T>>(i: T, j: T) -> T {
     i + j
 }
