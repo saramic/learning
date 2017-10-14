61dc8c1bf960f9fc55962862cdff36889e6ee536
Michael Milewski <saramic@gmail.com>
2017-10-15 06:02:54 +1100

    refactor to use regular expressions
    also use unwrap and a Some/None block

--- lang/rust/rust_in_action/grep-lite/src/main.rs
+++ lang/rust/rust_in_action/grep-lite/src/main.rs
@@ -1,3 +1,7 @@
+extern crate regex;
+
+use regex::Regex;
+
 fn main() {
     // PARAMETERS
     let context_lines = 2;
@@ -51,15 +55,19 @@ fn main() {
     }
     println!();
 
-    let search_term = "picture";
+    let re = Regex::new("picture").unwrap();
+
     let quote = "Every face, every shop, bedroom window, public-house, and
     dark square is a picture feverishly turned--in search of what?
     It is the same with books. What do we seek through millions of pages?";
 
     for (idx, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            let line_num = idx + 1;
-            println!("{}: {}", line_num, line);
+        match re.find(line) {
+            Some(_) => {
+                let line_num = idx + 1;
+                println!("{}: {}", line_num, line)
+            },
+            None => (),
         }
     }
 }
