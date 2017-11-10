eadb9686bfd1a3486d30b01c3a0209f014414f9e
Michael Milewski <saramic@gmail.com>
2017-11-11 06:45:29 +1100

    using Result to return from functions that are susceptible to failure

--- lang/rust/rust_in_action/globalerror/src/main.rs
+++ lang/rust/rust_in_action/globalerror/src/main.rs
@@ -1,29 +1,65 @@
 extern crate rand;
 use ::rand::Rng;
 
-static mut ERROR: isize = 0;
-
-struct File;
-
-#[allow(unused_variables)]
-fn read(f: &File, save_to: Vec<u8>) -> usize {
-    if rand::thread_rng().gen_weighted_bool(3) {
-        unsafe {
-            ERROR = 1;
-        }
-    }
-    0
+fn one_in(n: u32) -> bool {
+    rand::thread_rng().gen_weighted_bool(n)
 }
 
-#[allow(unused_mut)]
-fn main() {
-    let mut f = File;
-    let mut buffer = vec![];
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
 
-    read(&f, buffer);
-    unsafe {
-        if ERROR != 0 {
-            panic!("An error has occurred!")
-        }
+impl File {
+    fn new(name: &str) -> File {
+        File { name: String::from(name), data: Vec::new() }
     }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
+        let mut f = File::new(name);
+        f.data = data.clone();
+        f
+    }
+
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+        Ok(read_length)
+    }
+}
+
+fn  open(f: File) -> Result<File, String> {
+    if one_in(10_000) {
+        let err_msg = String::from("Permission denied");
+        return Err(err_msg);
+    }
+    Ok(f)
+}
+
+fn close(f: File) -> Result<File, String> {
+    if one_in(100_000) {
+        let err_msg = String::from("Interrupted by signal!");
+        return Err(err_msg);
+    }
+    Ok(f)
+}
+
+fn main() {
+    let f4_data: Vec<u8> = vec![114, 117, 115, 116, 33];
+    let mut f4 = File::new_with_data("4.txt", &f4_data);
+
+    let mut buffer: Vec<u8> = vec![];
+
+    f4 = open(f4).unwrap();
+    let f4_length = f4.read(&mut buffer).unwrap();
+    f4 = close(f4).unwrap();
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f4);
+    println!("{} is {} bytes long", &f4.name, f4_length);
+    println!("{}", text);
 }
