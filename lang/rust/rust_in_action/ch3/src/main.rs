14f38ddb3471738a632f999ec07479bc08089873
Michael Milewski <saramic@gmail.com>
2017-11-20 23:02:39 +1100

    trait having to implement Display

--- lang/rust/rust_in_action/ch3/src/main.rs
+++ lang/rust/rust_in_action/ch3/src/main.rs
@@ -1,9 +1,34 @@
+#![allow(dead_code)]
 #![allow(unused_variables)]
 
+use std::fmt;
+use std::fmt::{Display};
+
+#[derive(Debug,PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
 #[derive(Debug)]
 struct File {
     name: String,
     data: Vec<u8>,
+    state: FileState,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
 }
 
 impl File {
@@ -11,6 +36,7 @@ impl File {
         File {
             name: String::from(name),
             data: Vec::new(),
+            state: FileState::Closed,
         }
     }
 
