90d0d5859bf1e2e9271f15ee8fda90804a6e13c2
Michael Milewski <saramic@gmail.com>
2017-12-04 12:20:26 +1100

    part 2

--- dojo/adventofcode.com/day1/day_1.rb
+++ dojo/adventofcode.com/day1/day_1.rb
@@ -1,2 +1,3 @@
 digits = File.open("data.txt").read.chomp.chars.map(&:to_i); sum = 0; digits.each_with_index{|i, idx| sum += i if digits[(idx + 1) % digits.count] == i }; puts sum
 
+sum = 0; digits.each_with_index{|i, idx| sum += i if digits.rotate(digits.count/2)[idx] == i }; puts sum
