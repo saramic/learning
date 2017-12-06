b24f2801fd511647d5d2fed0113b81c337a84a20
Michael Milewski <saramic@gmail.com>
2017-12-06 22:48:45 +1100

    some minor improvements

--- framework/openCV/pyimagesearch_ppao/ch5_quiz.py
+++ framework/openCV/pyimagesearch_ppao/ch5_quiz.py
@@ -6,14 +6,12 @@ canvas = np.zeros((300, 300, 3), dtype = "uint8")
 green = (0, 255, 0)
 red = (0, 0, 255)
 
-offset = 10
-for i in range(1, canvas.shape[0], 10):
-    if offset == 10:
+for (col, x) in enumerate(range(1, canvas.shape[0], 10)):
+    for (row, y) in enumerate(range(1, canvas.shape[1], 20)):
         offset = 0
-    else:
-        offset = 10
-    for j in range(1, canvas.shape[1], 20):
-        cv2.rectangle(canvas, (i, j+offset), (i+9, j+offset+9), red, -1)
+        if col % 2 == 0:
+            offset = 10
+        cv2.rectangle(canvas, (x, y+offset), (x+9, y+offset+9), red, -1)
 
 (centerX, centerY) = (canvas.shape[1] // 2, canvas.shape[0] // 2)
 cv2.circle(canvas, (centerX, centerY), 50, green, -1)
