f95ef56aa7e7fc3817c44a105f94be92a43878d1
Michael Milewski <saramic@gmail.com>
2017-12-07 13:23:34 +1100

    make more generic for images and detectors

--- framework/openCV/pyimagesearch_gurus/0.1_setup/detect_faces.py
+++ framework/openCV/pyimagesearch_gurus/0.1_setup/detect_faces.py
@@ -1,10 +1,20 @@
+import argparse
 import cv2
 
-image = cv2.imread("orientation_example.jpg")
+# run like so
+#   python3 detect_faces.py -i orientation_example.jpg  -d haarcascade_frontalface_default.xml
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+ap.add_argument("-d", "--detector", required = True, help = "Name of detector\n/usr/local/Cellar/opencv/3.3.1_1/share/OpenCV/haarcascades/*.xml")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-detector = cv2.CascadeClassifier("/usr/local/Cellar/opencv/3.3.1_1/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml")
+detector = cv2.CascadeClassifier("/usr/local/Cellar/opencv/3.3.1_1/share/OpenCV/haarcascades/" + args["detector"])
+
 rects = detector.detectMultiScale(
         gray,
         scaleFactor = 1.05,
