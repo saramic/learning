c5355cd687ae37f98f369d5d266fffb69913dc63
Michael Milewski <saramic@gmail.com>
2017-12-23 15:29:04 +1100

    ch5 unsinged vs signed int

---
+++ lang/rust/rust_in_action/ch5-data-in-depth/int-vs-int.rs
@@ -0,0 +1,8 @@
+#![allow(overflowing_literals)]
+
+fn main() {
+    let a: u16 = 0b1100_0011_1100_0011;
+    let b: i16 = 0b1100_0011_1100_0011;
+    println!("a: {}", a);
+    println!("b: {}", b);
+}
