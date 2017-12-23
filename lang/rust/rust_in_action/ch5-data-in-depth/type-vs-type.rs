f2662646bdffdbc8b75acbac9105a1999887682f
Michael Milewski <saramic@gmail.com>
2017-12-23 15:58:24 +1100

    endianness

--- lang/rust/rust_in_action/ch5-data-in-depth/type-vs-type.rs
+++ lang/rust/rust_in_action/ch5-data-in-depth/type-vs-type.rs
@@ -1,4 +1,5 @@
 #![allow(overflowing_literals)]
+use std::mem;
 
 fn main() {
     println!("int vs int");
@@ -39,4 +40,26 @@ fn main() {
     let c: u8 = a + b;
     println!("200 + 200 = {}", c);
 
+    println!("");
+    println!("Endianness");
+
+    let big_endian: [u8; 4] = [
+        0xAA, // 1101_1101
+        0xBB, // 1100_1100
+        0xCC, // 1011_1011
+        0xDD, // 1010_1010
+    ];
+
+    let little_endian: [u8; 4] = [
+        0xDD, // 1010_1010
+        0xCC, // 1011_1011
+        0xBB, // 1100_1100
+        0xAA, // 1101_1101
+    ];
+
+    let (a, b): (i32, i32) = unsafe {
+        (mem::transmute(big_endian), mem::transmute(little_endian))
+    };
+
+    println!("{} vs {}", a, b);
 }
