780d2aa08b145cde92c3b3767076aec96188cf84
Michael Milewski <saramic@gmail.com>
2017-12-24 15:53:45 +1100

    day23 - all gotos removed and loops fixed
    still there is a problem as the wrong answer is being returned :(

--- dojo/adventofcode.com/day23/assembly_refactor.rb
+++ dojo/adventofcode.com/day23/assembly_refactor.rb
@@ -1,13 +1,9 @@
 #!/usr/bin/env ruby
-# assuming you have __goto__ and __label__ enabled in your ruby
-# http://patshaughnessy.net/2012/2/29/the-joke-is-on-us-how-ruby-1-9-supports-the-goto-statement
 
 # registers with initial values
 a, b, c, d, e, f, g, h = 1, 0, 0, 0, 0 ,0 ,0 ,0
 
-#  0 set b 84
 b = 84
-#  1 set c b
 c = b
 
 if (a != 0)
@@ -15,29 +11,19 @@ if (a != 0)
   c = b + 17_000
 end
 
-loop
+loop do
   f = 1
   d = 2
-  do
+  loop do
     e = 2
-    loop
-      g = d * e - b
-      # 14 jnz g 2
-      __goto__(:line_16) if g != 0
-      # 15 set f 0
-      f = 0
-      # 16 sub e -1
-      __label__(:line_16)
+    loop do
+      f = 0 if (d * e != b)
       e += 1
-      break if d != b
+      break if e != b
     end
-    # 20 sub d -1
     d += 1
-    # 21 set g d
-    g = d - b
-    # 23 jnz g -13
-  end while (d != b)
-
+    break if d != b
+  end
   h += 1 if f == 0
   break if b == c
   b += 17
