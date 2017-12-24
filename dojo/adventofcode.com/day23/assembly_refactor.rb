32603a772becb60e1d3e4f72be4a5a1ca82ef00a
Michael Milewski <saramic@gmail.com>
2017-12-24 15:34:26 +1100

    day23 identify a loop and print h which is the desired outcome

--- dojo/adventofcode.com/day23/assembly_refactor.rb
+++ dojo/adventofcode.com/day23/assembly_refactor.rb
@@ -10,53 +10,51 @@ b = 84
 #  1 set c b
 c = b
 
-if (a != 0) {
+if (a != 0)
   b = b * 100 + 10_0000
   c = b + 17_000
-}
+end
 
-#  8 set f 1
-__label__(:line_8)
-f = 1
-#  9 set d 2
-d = 2
-# 10 set e 2
-__label__(:line_10)
-e = 2
-# 11 set g d
-__label__(:line_11)
-g = d * e - b
-# 14 jnz g 2
-__goto__(:line_16) if g != 0
-# 15 set f 0
-f = 0
-# 16 sub e -1
-__label__(:line_16)
-e += 1
-# 17 set g e
-g = e - b
-# 19 jnz g -8
-__goto__(:line_11) if g != 0
-# 20 sub d -1
-d += 1
-# 21 set g d
-g = d - b
-# 23 jnz g -13
-__goto__(:line_10) if g != 0
-# 24 jnz f 2
-__goto__(:line_26) if f != 0
-# 25 sub h -1
-h += 1
-# 26 set g b
-__label__(:line_26)
-g = b - c
-# 28 jnz g 2
-__goto__(:line_30) if g != 0
-# 29 jnz 1 3
-__goto__(:line_32)
-# 30 sub b -17
-__label__(:line_30)
-b += 17
-# 31 jnz 1 -23
-__goto__(:line_8)
-__label__(:line_32)
+loop
+  f = 1
+  #  9 set d 2
+  d = 2
+  # 10 set e 2
+  __label__(:line_10)
+  e = 2
+  # 11 set g d
+  __label__(:line_11)
+  g = d * e - b
+  # 14 jnz g 2
+  __goto__(:line_16) if g != 0
+  # 15 set f 0
+  f = 0
+  # 16 sub e -1
+  __label__(:line_16)
+  e += 1
+  # 17 set g e
+  g = e - b
+  # 19 jnz g -8
+  __goto__(:line_11) if g != 0
+  # 20 sub d -1
+  d += 1
+  # 21 set g d
+  g = d - b
+  # 23 jnz g -13
+  __goto__(:line_10) if g != 0
+  # 24 jnz f 2
+  h += 1 if f == 0
+  # 26 set g b
+  __label__(:line_26)
+  g = b - c
+  # 28 jnz g 2
+  __goto__(:line_30) if g != 0
+  # 29 jnz 1 3
+  __goto__(:line_32)
+  # 30 sub b -17
+  __label__(:line_30)
+  b += 17
+  # 31 jnz 1 -23
+  __goto__(:line_8)
+end
+puts h
