6ad7d07611fceb5668a6098c86cf6fe987e4670c
Michael Milewski <saramic@gmail.com>
2017-12-24 15:36:36 +1100

    day23 - break if b == c

--- dojo/adventofcode.com/day23/assembly_refactor.rb
+++ dojo/adventofcode.com/day23/assembly_refactor.rb
@@ -42,19 +42,9 @@ loop
   g = d - b
   # 23 jnz g -13
   __goto__(:line_10) if g != 0
-  # 24 jnz f 2
+
   h += 1 if f == 0
-  # 26 set g b
-  __label__(:line_26)
-  g = b - c
-  # 28 jnz g 2
-  __goto__(:line_30) if g != 0
-  # 29 jnz 1 3
-  __goto__(:line_32)
-  # 30 sub b -17
-  __label__(:line_30)
+  break if b == c
   b += 17
-  # 31 jnz 1 -23
-  __goto__(:line_8)
 end
 puts h
