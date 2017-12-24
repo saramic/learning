d2202c9a63cbb1fc0eaa1163cb9bd926d252a4d2
Michael Milewski <saramic@gmail.com>
2017-12-24 14:46:35 +1100

    improve logging with a tick value

--- dojo/adventofcode.com/day23/script.rb
+++ dojo/adventofcode.com/day23/script.rb
@@ -3,12 +3,12 @@ output = {}
 commands = ARGF.read.split("\n")
 index = 0
 mul_count = 0
+tick = 0
 begin
   loop do
     index_offset = 1
     cmd, reg, o_val = commands[index].split
     val = output.has_key?(o_val) ? output[o_val] : o_val.to_i
-    #puts [index, cmd, reg, sprintf("%6s", o_val.to_s), sprintf("%6d", val), output].inspect
     #puts [output['e'], output['g']].inspect
     case cmd
     when 'set'
@@ -21,10 +21,12 @@ begin
     when 'jnz'
       index_offset = val if output[reg] != 0
     end
+    puts [tick, index, commands[index], output].inspect
     index += index_offset
     #puts output.inspect
     #puts mul_count
     #sleep(1)
+    tick += 1
   end
 rescue Exception => e
   puts e
