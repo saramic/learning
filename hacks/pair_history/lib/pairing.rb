1c33489a964b282edb3524cc25a95aa307ad3bee
Michael Milewski <saramic@gmail.com>
2018-05-26 10:44:19 +1000

    connected git gem to init

--- hacks/pair_history/lib/pairing.rb
+++ hacks/pair_history/lib/pairing.rb
@@ -1,5 +1,8 @@
+require 'git'
+
 class Pairing
   def initialize(git_working_directory, git_committers_names_yml)
+    @git = Git.open(git_working_directory)
   end
 
   def stats(output)
--- hacks/pair_history/spec/pairing_spec.rb
+++ hacks/pair_history/spec/pairing_spec.rb
@@ -1,11 +1,13 @@
 require 'pairing'
 
 RSpec.describe Pairing do
-  it 'takes a git woring directory and committers.yml to init' do
+  it 'passes the working_directory to Git gem' do
+    expect(Git).to receive(:open).with('working_directory')
     expect(Pairing.new('working_directory', 'commiters.yml')).to be_truthy
   end
 
   it 'outputs the pairing stats' do
+    expect(Git).to receive(:open).with('working_directory')
     pairing = Pairing.new('working_directory', 'commiters.yml')
     output = StringIO.new
     expect(output).to receive(:puts).with 'pairing stats'
