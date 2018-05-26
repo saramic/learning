40b71f54552fafe5d0fa09c049a834b114f5e183
Michael Milewski <saramic@gmail.com>
2018-05-26 11:34:58 +1000

    HACK yaml input for users

--- hacks/pair_history/lib/pairing.rb
+++ hacks/pair_history/lib/pairing.rb
@@ -1,11 +1,14 @@
 require 'git'
+require 'yaml'
 
 class Pairing
   def initialize(git_working_directory, git_committers_names_yml)
     @git = Git.open(git_working_directory)
+    @committers = YAML.load(File.open(git_committers_names_yml).read)
   end
 
   def stats(output)
     output.puts 'pairing stats'
+    output.puts @committers.keys.join(" | ")
   end
 end
--- hacks/pair_history/spec/pairing_spec.rb
+++ hacks/pair_history/spec/pairing_spec.rb
@@ -1,16 +1,41 @@
 require 'pairing'
 
 RSpec.describe Pairing do
-  it 'passes the working_directory to Git gem' do
-    expect(Git).to receive(:open).with('working_directory')
-    expect(Pairing.new('working_directory', 'commiters.yml')).to be_truthy
-  end
+  context 'integrate with actual git commiters yaml file' do
+    let(:git_committers_file) do
+      filename = 'committers.yml'
+      git_committers_file = Tempfile.new(filename).tap do |file|
+        committers_yaml = <<-EOF.strip_leading_spaces
+        ---
+        :User_1:
+          - "@user_1"
+        :User_2:
+          - "@user_2"
+        EOF
+        file.write(committers_yaml)
+        file.close
+      end
+    end
 
-  it 'outputs the pairing stats' do
-    expect(Git).to receive(:open).with('working_directory')
-    pairing = Pairing.new('working_directory', 'commiters.yml')
-    output = StringIO.new
-    expect(output).to receive(:puts).with 'pairing stats'
-    pairing.stats(output)
+    it 'passes the working_directory to Git gem' do
+      expect(Git).to receive(:open).with('working_directory')
+      expect(Pairing.new('working_directory', git_committers_file.path)).to be_truthy
+    end
+
+    it 'outputs the pairing stats' do
+      expect(Git).to receive(:open).with('working_directory')
+      pairing = Pairing.new('working_directory', git_committers_file.path)
+      output = StringIO.new # TODO any benefit or could this be a double
+      expect(output).to receive(:puts).with 'pairing stats'
+      expect(output).to receive(:puts).with 'User_1 | User_2'
+      pairing.stats(output)
+    end
   end
 end
+
+class String
+  def strip_leading_spaces
+    self.split("\n").map{|line| line.sub(/^\s+/, '') }.join("\n")
+  end
+end
+
