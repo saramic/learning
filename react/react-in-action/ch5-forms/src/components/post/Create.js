007e7f290fefd2aa1d49f8fa5e882550fa355987
Michael Milewski <saramic@gmail.com>
2018-09-24 19:28:27 +1000

    BOOK react-in ch5 post and submit handlers of a controlled input form

--- react/react-in-action/ch5-forms/src/components/post/Create.js
+++ react/react-in-action/ch5-forms/src/components/post/Create.js
@@ -6,11 +6,33 @@ class CreatePost extends Component {
   }
   constructor(props) {
     super(props)
+    this.state = { content: "" }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handlePostChange = this.handlePostChange.bind(this)
   }
+  handlePostChange(event) {
+    // console.log(event) // TODO uncomment to see events fire
+    console.log("handling an update to the post body!")
+    const content = event.target.value
+    this.setState(() => {
+      return {
+        content
+      }
+    })
+  }
+  handleSubmit() {
+    console.log("Handling submission")
+  }
+
   render () {
     return (
       <div className="create-post">
-        Create a post - coming (very) soon
+        <button onClick={this.handleSubmit}>Post</button>
+        <textarea
+          value={this.state.content}
+          onChange={this.handlePostChange}
+          placeholder="What's on your mind?"
+        />
       </div>
     )
   }
