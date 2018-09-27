07d25fec08e94e858814b87ccfff63cd8dc6369d
Michael Milewski <saramic@gmail.com>
2018-09-28 05:56:33 +1000

    BOOK react-in ch9 test handlePostChange

--- react/react-in-action/ch9-testing/src/components/post/Create.js
+++ react/react-in-action/ch9-testing/src/components/post/Create.js
@@ -1,7 +1,32 @@
+import PropTypes from "prop-types"
 import React, { Component } from "react"
 import { render } from "react-dom"
+import Filter from "bad-words"
 
-class Create extends Component {
+class CreatePost extends Component {
+  static propTypes = {
+    onSubmit: PropTypes.func.isRequired
+  }
+
+  constructor(props) {
+    super(props)
+    this.initialState = {
+      content: ""
+    }
+    this.state = this.initialState
+    this.filter = new Filter()
+    this.handlePostChange = this.handlePostChange.bind(this)
+  }
+
+  handlePostChange(event) {
+    const content = this.filter.clean(event.target.value)
+    this.setState(() => {
+      return {
+        content,
+      }
+    })
+  }
+
   render() {
     return (
       <div />
@@ -9,4 +34,4 @@ class Create extends Component {
   }
 }
 
-export default Create
+export default CreatePost
--- react/react-in-action/ch9-testing/src/components/post/Create.test.js
+++ react/react-in-action/ch9-testing/src/components/post/Create.test.js
@@ -10,4 +10,19 @@ describe("CreatePost", () => {
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
   })
+  test("handlePostChange", () => {
+    const props = { onSubmit: jest.fn() }
+    const mockEvent = { target: { value: "value" } }
+    CreatePost.prototype.setState = jest.fn(function(updater) {
+      this.state = Object.assign(this.state, updater(this.state))
+    })
+
+    const component = new CreatePost(props)
+    component.handlePostChange(mockEvent)
+    expect(component.setState).toHaveBeenCalled()
+    expect(component.setState.mock.calls.length).toEqual(1)
+    expect(component.state).toEqual({
+      content: mockEvent.target.value,
+    })
+  })
 })
