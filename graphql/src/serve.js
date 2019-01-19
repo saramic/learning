963f189c8a353ffe72a8df52cbfd44ba2249ae8d
Michael Milewski <saramic@gmail.com>
2019-01-19 11:16:32 +1100

    BOOK course on graphql

---
+++ graphql/src/serve.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+import schema from './schema'
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true,
+}))
+
+console.log('Starting the server ...')
+app.listen(4000)
