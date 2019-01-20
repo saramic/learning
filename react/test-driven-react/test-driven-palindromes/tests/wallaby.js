5d9bff54ce5be382efefaec0b9a910aee4c013ec
Michael Milewski <saramic@gmail.com>
2019-01-20 11:20:47 +1100

    BOOK assuming I can get a trial licence?
    used CMD-X to add extention
    waiting on reply https://twitter.com/saramic/status/1086772836209807360

---
+++ react/test-driven-react/test-driven-palindromes/tests/wallaby.js
@@ -0,0 +1,10 @@
+module.exports = function() {
+  return {
+    testFramework: 'jest',
+    env: {
+      type: 'node',
+    },
+    tests: ['tests/**/*.test.js'],
+    files: ['**/*.js', '!node_modules/**/**', '!**/*.test.js', '!**/.*'],
+  }
+}
