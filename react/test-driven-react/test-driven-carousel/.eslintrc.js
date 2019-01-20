95ec7fc728fb51b1bf1bc92e874b4f2a72c40419
Michael Milewski <saramic@gmail.com>
2019-01-20 12:00:25 +1100

    BOOK eslint setup
    npm install --save-dev eslint prettier-eslint-cli eslint-plugin-jest
    although VSCode formatting is double quotes for string which conflicted with
    eslint setup so that is commented out

---
+++ react/test-driven-react/test-driven-carousel/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ["eslint:recommended"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    node: true
+  },
+  rules: {
+    // quotes: ["error", "single", { avoidEscape: true }],
+    "comma-dangle": ["error", "always-multiline"]
+  }
+};
