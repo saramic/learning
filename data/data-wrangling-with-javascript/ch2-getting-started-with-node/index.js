1b512d6adb7e3310cd7b89fd55d0d9e9fd2bcb0a
Michael Milewski <saramic@gmail.com>
2019-01-20 19:15:29 +1100

    BOOK simple express app
    run
    node index.js
    
    added express
    npm install --save express

---
+++ data/data-wrangling-with-javascript/ch2-getting-started-with-node/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("This is a web page!");
+});
+
+app.listen(3000, () => {
+  console.log("Web server listening on port 3000!");
+});
