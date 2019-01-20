7d098e81da9f02d136a22ae1ac5f40dcb78dd1f5
Michael Milewski <saramic@gmail.com>
2019-01-20 11:38:42 +1100

    BOOK :wrench: a first JSX test
    npm install --save-dev jest babel-jest
    npm install --save-dev babel-core@^7.0.0-bridge.0
    npm install react
    npm install --save-dev react-dom

---
+++ react/test-driven-react/test-driven-carousel/src/tests/hello.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+describe("JSX", () => {
+  it("calls React.createElement", () => {
+    const createElementSpy = jest.spyOn(React, "createElement");
+    <h1>Hello, JSX!</h1>;
+    expect(createElementSpy).toHaveBeenCalledWith("h1", null, "Hello, JSX!");
+  });
+});
