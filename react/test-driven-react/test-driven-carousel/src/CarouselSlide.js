23c05931973ee4b22905d39ac74ec81c5febb18d
Michael Milewski <saramic@gmail.com>
2019-01-20 12:31:27 +1100

    BOOK got started on carousel slide

---
+++ react/test-driven-react/test-driven-carousel/src/CarouselSlide.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CarouselSlide = () => (
+  <figure>
+    <img />
+    <figcaption />
+  </figure>
+);
+
+export default CarouselSlide;
---
+++ react/test-driven-react/test-driven-carousel/src/tests/CarouselSlide.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { shallow } from "enzyme";
+import CarouselSlide from "../CarouselSlide";
+
+describe("CarouseSlide", () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<CarouselSlide />);
+  });
+
+  it("renders a <figure>", () => {
+    expect(wrapper.type()).toBe("figure");
+  });
+
+  it("renders an <img> and a <figcaption> as children", () => {
+    expect(wrapper.childAt(0).type()).toBe("img");
+    expect(wrapper.childAt(1).type()).toBe("figcaption");
+  });
+});
