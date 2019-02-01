ad7c838b1194560f1758f8afead0367e5fe0466b
Michael Milewski <saramic@gmail.com>
2019-02-01 20:50:26 +1100

    BOOK :bulb: add example page

--- react/test-driven-react/test-driven-carousel/example/index.js
+++ react/test-driven-react/test-driven-carousel/example/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Carousel from "../src/Carousel";
+import slides from "./slides";
 
 const container = document.createElement("div");
 document.body.appendChild(container);
-ReactDOM.render(<h1>Hello, Webpack!</h1>, container);
+ReactDOM.render(<Carousel slides={slides} />, container);
