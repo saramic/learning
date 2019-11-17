6f207f67f8dd62ce63c47c3d38d879ba5f686c78
Michael Milewski <saramic@gmail.com>
2019-11-17 13:02:34 +1100

    [LCD] 3 is ready
    also some gsub magic
    
    Co-authored-by: Kirsten Comandich <kcomandich@gmail.com>
    Co-authored-by: Mel Kaulfuss <melissacor3lfuss@gmail.com>
    Co-authored-by: Robin Klaus <rkdunbar99@gmail.com>

--- dojo/ruby_lcd_numbers/spec/lcd_spec.rb
+++ dojo/ruby_lcd_numbers/spec/lcd_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Lcd do
  EO_DIGIT
     expect(Lcd.d(8)).to eq(result)
   end
+
+  it "displays 3" do
+    result = <<-EO_DIGIT
+ --
+   |
+ --
+   |
+ --
+ EO_DIGIT
+    expect(Lcd.d(3)).to eq(result)
+  end
 end
