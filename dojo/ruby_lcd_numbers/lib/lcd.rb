e6e8f62728ce395e0dbb10f158e02492f9548630
Michael Milewski <saramic@gmail.com>
2019-11-17 14:30:22 +1100

    [LCD] line by line
    Co-authored-by: Kirsten Comandich <kcomandich@gmail.com>
    Co-authored-by: Mel Kaulfuss <melissacor3lfuss@gmail.com>
    Co-authored-by: Robin Klaus <rkdunbar99@gmail.com>

--- dojo/ruby_lcd_numbers/lib/lcd.rb
+++ dojo/ruby_lcd_numbers/lib/lcd.rb
@@ -1,21 +1,29 @@
 class Lcd
+  THREE = [
+    " -- ",
+    "   |",
+    " -- ",
+    "   |",
+    " -- ",
+  ].join("\n")
+
+  EIGHT = [
+    " -- ",
+    "|  |",
+    " -- ",
+    "|  |",
+    " -- ",
+  ].join("\n")
+
+  NUMBERS = [0, 1, 2, THREE, 4, 5, 6, 7, EIGHT, 9]
+
   def self.d(number)
-    if number == 3
-      <<-EO_DIGIT.gsub(/^( ){7}/, "")
-        --
-          |
-        --
-          |
-        --
-    EO_DIGIT
-    else
-      <<-EO_DIGIT.gsub(/^(  )+/, "")
-       --
-      |  |
-       --
-      |  |
-       --
-    EO_DIGIT
+    output = Array.new(5, "")
+    for line_index in 0..4
+      output[line_index] = number.to_s.chars.map do |digit|
+        NUMBERS[digit.to_i].split("\n")[line_index]
+      end.join("  ")
     end
+    output.join("\n") + "\n"
   end
 end
