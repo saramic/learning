28ecd1f70f4dc72d9ec8b6d582e3d00d9450a234
Michael Milewski <saramic@gmail.com>
2019-11-17 13:49:58 +1100

    better indent the rspec

--- dojo/ruby_lcd_numbers/spec/lcd_spec.rb
+++ dojo/ruby_lcd_numbers/spec/lcd_spec.rb
@@ -1,23 +1,23 @@
 RSpec.describe Lcd do
   it "displays 8" do
-    result = <<-EO_DIGIT
- --
-|  |
- --
-|  |
- --
- EO_DIGIT
+    result = <<-EO_DIGIT.gsub(/^( ){6}/, "")
+       --
+      |  |
+       --
+      |  |
+       --
+    EO_DIGIT
     expect(Lcd.d(8)).to eq(result)
   end
 
   it "displays 3" do
-    result = <<-EO_DIGIT
- --
-   |
- --
-   |
- --
- EO_DIGIT
+    result = <<-EO_DIGIT.gsub(/^( ){6}/, "")
+       --
+         |
+       --
+         |
+       --
+    EO_DIGIT
     expect(Lcd.d(3)).to eq(result)
   end
 end
