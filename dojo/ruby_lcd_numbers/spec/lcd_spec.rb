4f1169b9170945623b2ee433ed61e8edc580594f
Michael Milewski <saramic@gmail.com>
2019-11-17 14:30:22 +1100

    [LCD] line by line
    Co-authored-by: Kirsten Comandich <kcomandich@gmail.com>
    Co-authored-by: Mel Kaulfuss <melissacor3lfuss@gmail.com>
    Co-authored-by: Robin Klaus <rkdunbar99@gmail.com>

--- dojo/ruby_lcd_numbers/spec/lcd_spec.rb
+++ dojo/ruby_lcd_numbers/spec/lcd_spec.rb
@@ -1,23 +1,34 @@
 RSpec.describe Lcd do
   it "displays 8" do
     result = <<-EO_DIGIT.gsub(/^( ){6}/, "")
-       --
+       -- 
       |  |
-       --
+       -- 
       |  |
-       --
+       -- 
     EO_DIGIT
     expect(Lcd.d(8)).to eq(result)
   end
 
   it "displays 3" do
     result = <<-EO_DIGIT.gsub(/^( ){6}/, "")
-       --
+       -- 
          |
-       --
+       -- 
          |
-       --
+       -- 
     EO_DIGIT
     expect(Lcd.d(3)).to eq(result)
+  end
+
+  it "displays 38" do
+    result = <<-EO_DIGIT.gsub(/^( ){6}/, "")
+       --    -- 
+         |  |  |
+       --    -- 
+         |  |  |
+       --    -- 
+    EO_DIGIT
+    expect(Lcd.d(38)).to eq(result)
   end
 end
