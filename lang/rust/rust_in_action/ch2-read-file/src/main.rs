653c084a63077bca0546196d976025d7ca6aa8eb
Michael Milewski <saramic@gmail.com>
2019-12-21 15:13:55 +1100

    [BOOK] .lines from BufReader

--- lang/rust/rust_in_action/ch2-read-file/src/main.rs
+++ lang/rust/rust_in_action/ch2-read-file/src/main.rs
@@ -2,8 +2,8 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
-fn main() {
-    let f = File::open("readme.md").unwrap();
+fn manually_iterate_lines_in_file() {
+    let f = File::open("README.md").unwrap();
     let mut reader = BufReader::new(f);
 
     let mut line = String::new();
@@ -16,4 +16,21 @@ fn main() {
         println!("{} ({} bytes long)", line, len);
         line.truncate(0);
     }
+}
+
+fn lines_hepler() {
+    let f = File::open("README.md").unwrap();
+    let reader = BufReader::new(f);
+
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        println!("{} ({} bytes long)", line, line.len());
+    }
+}
+
+fn main() {
+    println!("------ manually iterate file");
+    manually_iterate_lines_in_file();
+    println!("------ iterate file with helper");
+    lines_hepler();
 }
