6b52b34db8326bf129a37bfa8d338d726a42989c
Michael Milewski <saramic@gmail.com>
2019-12-21 13:42:41 +1100

    [dojo] some leet and adventofcode

--- dojo/adventofcode.com/2019/day_01.rb
+++ dojo/adventofcode.com/2019/day_01.rb
@@ -4,5 +4,12 @@ space_modules = ARGF.read.split("\n")
 fuel = space_modules.map do |space_module|
   (space_module.to_i / 3).floor - 2
 end
+
+def mass_to_fuel(space_module)
+  (space_module.to_i / 3).floor - 2
+end
+
+fuel = space_modules.map(&mass_to_fuel)
+
 puts fuel.sum
 
