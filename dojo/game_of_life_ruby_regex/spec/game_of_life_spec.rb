45cdadc15bb86a124ecb6cf47c7d8f98f990a816
Michael Milewski <saramic@gmail.com>
2020-01-29 08:56:07 +1100

    [GOL] fix tests
    neigbours takes an array not a string now, and a nasty bug in passing a
    variable as the string name of the variable :facepalm:

--- dojo/game_of_life_ruby_regex/spec/game_of_life_spec.rb
+++ dojo/game_of_life_ruby_regex/spec/game_of_life_spec.rb
@@ -20,14 +20,18 @@ describe GameOfLife do
     expect( GameOfLife.tick(world_start) ).to eq ""
   end
 
-  xit "3 cells in a row turn to 3 cells vertical" do
-    world_start = "***"
+  it "3 cells in a row turn to 3 cells vertical (if padded)" do
+    world_start = <<~EOF.gsub(/\w/, '').chomp
+    ...
+    ***
+    ...
+    EOF
     world_end = <<~EOF.gsub(/\w/, '').chomp
     .*.
     .*.
     .*.
     EOF
-    expect( GameOfLife.tick("world_start") ).to eq world_end
+    expect( GameOfLife.tick(world_start) ).to eq world_end
   end
 
   it "processes cells below, above, on line for multiple lines" do
@@ -42,38 +46,38 @@ describe GameOfLife do
     ......
     EOF
     expect(GameOfLife).to receive(:process)
-      .with(world_with_neighbours.slice(0,3).map{|row| row.slice(0,3) }.join("\n"))
+      .with(world_with_neighbours.slice(0,3).map{|row| row.slice(0,3).chars })
     expect(GameOfLife).to receive(:process)
-      .with(world_with_neighbours.slice(0,3).map{|row| row.slice(1,3) }.join("\n"))
+      .with(world_with_neighbours.slice(0,3).map{|row| row.slice(1,3).chars })
     expect(GameOfLife).to receive(:process)
-      .with(world_with_neighbours.slice(0,3).map{|row| row.slice(2,3) }.join("\n"))
+      .with(world_with_neighbours.slice(0,3).map{|row| row.slice(2,3).chars })
     expect(GameOfLife).to receive(:process)
-      .with(world_with_neighbours.slice(0,3).map{|row| row.slice(3,3) }.join("\n"))
+      .with(world_with_neighbours.slice(0,3).map{|row| row.slice(3,3).chars })
     expect(GameOfLife).to receive(:process)
-      .with(world_with_neighbours.slice(1,3).map{|row| row.slice(0,3) }.join("\n"))
+      .with(world_with_neighbours.slice(1,3).map{|row| row.slice(0,3).chars })
     expect(GameOfLife).to receive(:process)
-      .with(world_with_neighbours.slice(1,3).map{|row| row.slice(1,3) }.join("\n"))
+      .with(world_with_neighbours.slice(1,3).map{|row| row.slice(1,3).chars })
     expect(GameOfLife).to receive(:process)
-      .with(world_with_neighbours.slice(1,3).map{|row| row.slice(2,3) }.join("\n"))
+      .with(world_with_neighbours.slice(1,3).map{|row| row.slice(2,3).chars })
     expect(GameOfLife).to receive(:process)
-      .with(world_with_neighbours.slice(1,3).map{|row| row.slice(3,3) }.join("\n"))
+      .with(world_with_neighbours.slice(1,3).map{|row| row.slice(3,3).chars })
     GameOfLife.tick(".**.\n*..*")
   end
 
   it "process cells for rule 1 any live cells with 2 or 3 survives" do
-    expect(GameOfLife.process("...\n.*.\n...")).to eq "." # dies
-    expect(GameOfLife.process("*..\n.*.\n...")).to eq "." # dies
-    expect(GameOfLife.process("**.\n.*.\n...")).to eq "*" # lives
-    expect(GameOfLife.process("***\n.*.\n...")).to eq "*" # lives
-    expect(GameOfLife.process("**.\n.*.\n*..")).to eq "*" # lives
-    expect(GameOfLife.process("**.\n.*.\n**.")).to eq "." # dies
+    expect(GameOfLife.process("...\n.*.\n...".split("\n").map(&:chars))).to eq "." # dies
+    expect(GameOfLife.process("*..\n.*.\n...".split("\n").map(&:chars))).to eq "." # dies
+    expect(GameOfLife.process("**.\n.*.\n...".split("\n").map(&:chars))).to eq "*" # lives
+    expect(GameOfLife.process("***\n.*.\n...".split("\n").map(&:chars))).to eq "*" # lives
+    expect(GameOfLife.process("**.\n.*.\n*..".split("\n").map(&:chars))).to eq "*" # lives
+    expect(GameOfLife.process("**.\n.*.\n**.".split("\n").map(&:chars))).to eq "." # dies
   end
 
   it "process cells for rule 2 any dead cells with 3 becomes live" do
-    expect(GameOfLife.process("...\n...\n...")).to eq "." # dead
-    expect(GameOfLife.process("*..\n...\n...")).to eq "." # dead
-    expect(GameOfLife.process("**.\n...\n...")).to eq "." # dead
-    expect(GameOfLife.process("***\n...\n...")).to eq "*" # lives
-    expect(GameOfLife.process("**.\n*.*\n...")).to eq "." # dead
+    expect(GameOfLife.process("...\n...\n...".split("\n").map(&:chars))).to eq "." # dead
+    expect(GameOfLife.process("*..\n...\n...".split("\n").map(&:chars))).to eq "." # dead
+    expect(GameOfLife.process("**.\n...\n...".split("\n").map(&:chars))).to eq "." # dead
+    expect(GameOfLife.process("***\n...\n...".split("\n").map(&:chars))).to eq "*" # lives
+    expect(GameOfLife.process("**.\n*.*\n...".split("\n").map(&:chars))).to eq "." # dead
   end
 end
