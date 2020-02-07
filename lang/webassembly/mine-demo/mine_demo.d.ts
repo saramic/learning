0a0843be0b95bd0aff7a66f406bceefcd3fed708
Michael Milewski <saramic@gmail.com>
2020-02-07 20:57:03 +1100

    [WASM] build here as no cargo on netlify

---
+++ lang/webassembly/mine-demo/mine_demo.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class WasmMine {
+  free(): void;
+/**
+* @param {string} text 
+*/
+  constructor(text: string);
+/**
+* @param {string} text 
+* @returns {any} 
+*/
+  static wasm_mine(text: string): any;
+}
---
+++ lang/webassembly/mine-demo/mine_demo_bg.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_wasmmine_free(a: number): void;
+export function wasmmine_new(a: number, b: number): number;
+export function wasmmine_wasm_mine(a: number, b: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
