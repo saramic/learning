1ca88ac40d3731e3fa8be5cbc5b7eade334e63ff
Michael Milewski <saramic@gmail.com>
2020-02-10 18:24:16 +1100

    [WASM] a nasty rust program that accesses your public key
    first and last 50 characters and prints it out. Next step is to run it
    under WASM and see the light weight sandboxing stop it from accessing
    the file.

--- lang/webassembly/wasm_fileaccess/src/main.rs
+++ lang/webassembly/wasm_fileaccess/src/main.rs
@@ -1,3 +1,22 @@
+extern crate dirs;
+
+use std::fs;
+
+fn some_fingerprint(path: String) {
+    let contents =
+        fs::read_to_string(path + "/.ssh/id_rsa").expect("Something went wrong reading the file");
+
+    println!(
+        "with fingerprint:\n{}\n...\n{}",
+        &contents[..50],
+        &contents[contents.len() - 50..contents.len()]
+    )
+}
+
 fn main() {
     println!("Hello, world!");
+    match dirs::home_dir() {
+        Some(path) => some_fingerprint(path.display().to_string()),
+        None => (),
+    }
 }
