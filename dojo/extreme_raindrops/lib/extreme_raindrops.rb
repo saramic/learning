4e61e94f8e0cec9e5321372351cf88b5dc8aee0c
Michael Milewski <saramic@gmail.com>
2021-01-17 16:51:31 +1100

    Not all g materials are glass
    ruby -I lib/ -e 'require "extreme_raindrops"; glass = ExtremeRaindrops.new("g"); (1..100).to_a.each{|num| puts glass.rain_drops(num) } '
    
    Co-authored-by: Bence Fulop <fbence90@gmail.com>
    Co-authored-by: Christine Schorn <cschorn91@gmail.com>
    Co-authored-by: Lily Tan <lilyt2995@gmail.com>
    Co-authored-by: Mathieu Longe <mathieu.longe@orange.fr>

--- dojo/extreme_raindrops/lib/extreme_raindrops.rb
+++ dojo/extreme_raindrops/lib/extreme_raindrops.rb
@@ -1,2 +1,45 @@
 class ExtremeRaindrops
+  RAINDROPS = {
+    3 => "a",
+    5 => "e",
+    7 => "i",
+    11 => "o",
+    13 => "u",
+    17 => "y"
+  }
+
+  def initialize(material = "plastic")
+    @material = material
+  end
+  
+  def rain_drops(num)
+    output = ""
+    RAINDROPS.keys.each do |key|
+      output += resonance(:sound, key) if num % key == 0
+    end
+
+    output += num.to_s if output.empty?
+    output
+  end
+
+  private
+
+  def sound(resonant_sound)
+    pre = "l"
+    post = "ng"
+    if @material =~ /glass/ # thanks Mathieu
+      pre = "d"
+      post = "ck"
+    end
+    [
+      @material[0].upcase,
+      pre,
+      resonant_sound,
+      post
+    ].join
+  end
+
+  def resonance(surface, frequency)
+    method(surface).call(RAINDROPS[frequency])
+  end
 end
--- dojo/extreme_raindrops/spec/extreme_raindrops_spec.rb
+++ dojo/extreme_raindrops/spec/extreme_raindrops_spec.rb
@@ -1,7 +1,47 @@
 require 'extreme_raindrops'
 
 RSpec.describe ExtremeRaindrops do
-  it "does shit" do
-    expect(true).to be_truthy
+  [
+    { input: 1, output: "1"},
+    { input: 3, output: "Plang"},
+    { input: 5, output: "Pleng"},
+    { input: 7, output: "Pling"},
+    { input: 11, output: "Plong"},
+    { input: 13, output: "Plung"},
+    { input: 17, output: "Plyng"},
+    { input: 15, output: "PlangPleng"},
+    { input: 21, output: "PlangPling"},
+    { input: 63, output: "PlangPling"},
+    { input: 255255, output: "PlangPlengPlingPlongPlungPlyng"},
+  ].each do |args|
+    it "returns #{args[:output].inspect} for the input #{args[:input].inspect}" do
+    plastic_drop = ExtremeRaindrops.new
+      expect(
+        plastic_drop.rain_drops(args[:input])
+        ).to eq(args[:output])
+    end
   end
+
+  it "can initialize an extreme raindrop with a material" do
+    brick_drop = ExtremeRaindrops.new("brick")
+    expect(brick_drop.rain_drops(255255)).to eq("BlangBlengBlingBlongBlungBlyng")    
+  end
+
+  it "can initialize an extreme raindrop with a material glass" do
+    glass_drop = ExtremeRaindrops.new("glass")
+    expect(glass_drop.rain_drops(255255)).to eq("GdackGdeckGdickGdockGduckGdyck")    
+  end
+
+
+  it "can initialize an extreme raindrop with a material cermaic" do
+    glass_drop = ExtremeRaindrops.new("cermaic")
+    expect(glass_drop.rain_drops(255255)).to eq("ClangClengClingClongClungClyng")    
+  end
+
+
+  it "can initialize an extreme raindrop with a material wood" do
+    glass_drop = ExtremeRaindrops.new("woood")
+    expect(glass_drop.rain_drops(255255)).to eq("WlangWlengWlingWlongWlungWlyng")    
+  end
+
 end
