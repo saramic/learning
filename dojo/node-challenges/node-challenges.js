997374a6a8a8cc3aad0424669ddcdd1b19703605
Michael Milewski <saramic@gmail.com>
2021-10-04 15:29:06 +1100

    nested sumAll implementation

--- dojo/node-challenges/node-challenges.js
+++ dojo/node-challenges/node-challenges.js
@@ -2,4 +2,14 @@ function isEven(number) {
   return number % 2 === 0;
 }
 
-export { isEven }; // eslint-disable-line import/prefer-default-export
+function sumAll(arr) {
+  // NOTE: non nested would work
+  // return arr.reduce((prev, curr) => prev + curr, 0);
+  return arr.reduce(
+    (prev, element) =>
+      prev + (element instanceof Array ? sumAll(element) : element),
+    0
+  );
+}
+
+export { isEven, sumAll }; // eslint-disable-line import/prefer-default-export
--- dojo/node-challenges/node-challenges.spec.js
+++ dojo/node-challenges/node-challenges.spec.js
@@ -1,8 +1,23 @@
-import { isEven } from "./node-challenges";
+import { isEven, sumAll } from "./node-challenges";
 
 describe("isEven", () => {
   test("checks a number if it is even", () => {
     expect(isEven(5)).toBeFalsy;
     expect(isEven(4)).toBeTruthy;
+  });
+});
+
+describe("sumAll", () => {
+  test("sums a plain old array", () => {
+    expect(sumAll([1, 2, 3])).toBe(6);
+  });
+  test("sums a blank array", () => {
+    expect(sumAll([])).toBe(0);
+  });
+  test("sums negative numbers", () => {
+    expect(sumAll([2, 2, -3, -1])).toBe(0);
+  });
+  test("sums nested numbers", () => {
+    expect(sumAll([1, 2, 3, [4, 5, [6, 7], 8], 9])).toBe(45);
   });
 });
