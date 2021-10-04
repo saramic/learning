2cf1dc052e989859df37c29c2ac2aeda3345ab9e
Michael Milewski <saramic@gmail.com>
2021-10-04 16:07:37 +1100

    stringiy objectg implementation

--- dojo/node-challenges/node-challenges.js
+++ dojo/node-challenges/node-challenges.js
@@ -12,4 +12,27 @@ function sumAll(arr) {
   );
 }
 
-export { isEven, sumAll }; // eslint-disable-line import/prefer-default-export
+function stringify(obj) {
+  const output = [];
+  if (obj instanceof Array) {
+    output.push(`[${obj.map((element) => stringify(element))}]`);
+  } else if (obj instanceof Object) {
+    output.push(
+      `{${Object.keys(obj)
+        .map((key) => `"${key}":${stringify(obj[key])}`)
+        .join(",")} }`
+    );
+  }
+  if (typeof obj === "string") {
+    output.push(`"${obj}"`);
+  }
+  if (typeof obj === "number") {
+    output.push(obj);
+  }
+  if (typeof obj === "boolean") {
+    output.push(obj ? "true" : "false");
+  }
+  return output.join();
+}
+
+export { isEven, sumAll, stringify }; // eslint-disable-line import/prefer-default-export
--- dojo/node-challenges/node-challenges.spec.js
+++ dojo/node-challenges/node-challenges.spec.js
@@ -1,4 +1,4 @@
-import { isEven, sumAll } from "./node-challenges";
+import { isEven, sumAll, stringify } from "./node-challenges";
 
 describe("isEven", () => {
   test("checks a number if it is even", () => {
@@ -19,5 +19,29 @@ describe("sumAll", () => {
   });
   test("sums nested numbers", () => {
     expect(sumAll([1, 2, 3, [4, 5, [6, 7], 8], 9])).toBe(45);
+  });
+});
+
+describe("stringify", () => {
+  test("stringify an empty hash", () => {
+    expect(stringify({}).replace(/\s+/g, "")).toEqual("{}");
+  });
+  test("a hash with all the things", () => {
+    expect(
+      stringify({
+        foo: "bar",
+        num: 7,
+        nested: { going: "deeper" },
+        arr: [1, 2],
+      }).replace(/\s+/g, "")
+    ).toEqual('{"foo":"bar","num":7,"nested":{"going":"deeper"},"arr":[1,2]}');
+  });
+  test("an object with a boolean", () => {
+    expect(stringify({ a: false }).replace(/\s+/g, "")).toEqual('{"a":false}');
+  });
+  test("an object with a boolean", () => {
+    expect(stringify([false, "hello", 1]).replace(/\s+/g, "")).toEqual(
+      '[false,"hello",1]'
+    );
   });
 });
