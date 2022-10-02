c9d32541fb80fd492df4295cdc06890fc4da6b98
Michael Milewski <saramic@gmail.com>
2022-10-02 19:23:27 +1100

    Rails new minimal with no active record
    Initially tried with rails 7.0.4
      gem update rails
      rails _7.0.4_ ...
      BUT: this failed for adding rom-rails later on
        Bundler could not find compatible versions for gem "railties":
        rails (~> 7.0.4) was resolved to 7.0.4, which depends on
          railties (= 7.0.4)
        rom-rails was resolved to 0.7.0, which depends on
          railties (< 5.0, >= 3.0)
    
    so:
      gem install rails -v 6.1.7
      new rails-meet-rom _6.1.7_ \
        --minimal \
        --skip-active-record \
        --skip-spring \
        --skip-test
    and
      asdf local postgres 12.8
      asdf local ruby 3.2.0-preview1

--- framework/rom/rails-meet-rom/config/routes.rb
+++ framework/rom/rails-meet-rom/config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
