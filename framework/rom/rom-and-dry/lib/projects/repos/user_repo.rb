84917db392a51de01c94d9d67099fecdfe542beb
Michael Milewski <saramic@gmail.com>
2022-10-02 17:04:44 +1100

    Add all and create methods to repo
    make demo
      [] # no users
      [
        #<ROM::Struct::User id=1 first_name="Michael" last_name="Milewski"
          age=46
          created_at=2022-10-02 17:04:26.918782 +1100
          updated_at=2022-10-02 17:04:26.918782 +1100>
      ] # have 1 user created and returned
    
      undefined method `full_name' # next part there is no full_name method

--- framework/rom/rom-and-dry/lib/projects/repos/user_repo.rb
+++ framework/rom/rom-and-dry/lib/projects/repos/user_repo.rb
@@ -1,6 +1,17 @@
 module Projects
   module Repos
     class UserRepo < ROM::Repository[:users]
+      commands :create,
+      use: :timestamps,
+      plugins_options: {
+        timestamps: {
+          timestamps: %i(created_at updated_at)
+        }
+      }
+
+      def all
+        users.to_a
+      end
     end
   end
 end
