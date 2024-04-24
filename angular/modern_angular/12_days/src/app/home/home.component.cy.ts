ede4a02006d7718c53ac85e066e80bad1cbb5698
Michael Milewski <saramic@gmail.com>
2024-04-25 09:43:09 +1000

    Basic angular app following angular.dev tute
    at:
      https://angular.dev/tutorials/first-app
    
    with:
      make ng-dev-home-app
    
      ng new ng-dev-home-app \
        --defaults \
        --standalone \
        --routing

---
+++ angular/modern_angular/12_days/src/app/home/home.component.cy.ts
@@ -0,0 +1,7 @@
+import { HomeComponent } from './home.component'
+
+describe('HomeComponent', () => {
+  it('should mount', () => {
+    cy.mount(HomeComponent)
+  })
+})
---
+++ angular/modern_angular/12_days/src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
