34dd1308f9f36db5268b95620396a544caf82400
Michael Milewski <saramic@gmail.com>
2024-04-25 15:52:56 +1000

    Switch out to using http service
    following:
      https://angular.dev/tutorials/first-app/14-http
    
    with:
      npm install -g json-server
      json-server --watch db.json
    
      using fetch
      but could switch out to Angular HttpClient

--- angular/ng-dev-home-app/src/app/details/details.component.ts
+++ angular/ng-dev-home-app/src/app/details/details.component.ts
@@ -61,7 +61,10 @@ export class DetailsComponent {
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    this.housingService.getHousingLocationById(housingLocationId)
+      .then((housingLocation) => {
+        this.housingLocation = housingLocation;
+      });
   }
 
   submitApplication() {
--- angular/ng-dev-home-app/src/app/home/home.component.ts
+++ angular/ng-dev-home-app/src/app/home/home.component.ts
@@ -34,8 +34,11 @@ export class HomeComponent {
   filteredLocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList
+    this.housingService.getAllHousingLocations()
+    .then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList
+    });
   }
 
   filterResults(text: string) {
