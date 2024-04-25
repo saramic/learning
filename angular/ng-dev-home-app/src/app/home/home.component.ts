ce6d7b270e5ff634d8733d5d5d56656c338de376
Michael Milewski <saramic@gmail.com>
2024-04-25 10:16:31 +1000

    Added a new component for housing location
    following:
      https://angular.dev/tutorials/first-app/03-HousingLocation
    
    with:
      ng generate component housingLocation --inline-template --skip-tests

--- angular/ng-dev-home-app/src/app/home/home.component.ts
+++ angular/ng-dev-home-app/src/app/home/home.component.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
-    <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
-    </form>
+    <section>
+      <form>
+        <input type="text" placeholder="Filter by city">
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+    <section class="results">
+      <app-housing-location></app-housing-location>
+    </section>
   `,
   styleUrl: './home.component.css'
 })
---
+++ angular/ng-dev-home-app/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+
+}
