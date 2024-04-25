07a46e5eb9d525a36676b986f08ca502f5ffaffc
Michael Milewski <saramic@gmail.com>
2024-04-25 15:39:07 +1000

    Bind a filter results to the input
    following:
      https://angular.dev/tutorials/first-app/13-search

--- angular/ng-dev-home-app/src/app/home/home.component.ts
+++ angular/ng-dev-home-app/src/app/home/home.component.ts
@@ -11,14 +11,18 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city">
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter>
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >Search</button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
         [housingLocation]="housingLocation"
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
       ></app-housing-location>
     </section>
   `,
@@ -27,8 +31,21 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService)
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList
+  }
+
+  filterResults(text: string) {
+    if(!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      (housingLocation) => housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    )
   }
 }
