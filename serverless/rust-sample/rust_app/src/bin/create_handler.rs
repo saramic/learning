ce419afd1cc6c52e6b362f4839905cb9d0ebf3d9
Michael Milewski <saramic@gmail.com>
2024-05-22 21:43:10 +1000

    Naive error scenario test added
    as per:
      https://www.youtube.com/watch?v=UI_4jlCHkdc&list=PLCOG9xkUD90KQ1IPQT_m1NbPRXXRFb63s&index=3

--- serverless/rust-sample/rust_app/src/bin/create_handler.rs
+++ serverless/rust-sample/rust_app/src/bin/create_handler.rs
@@ -148,4 +148,49 @@ mod tests {
         assert_eq!(response.status(), 200);
         assert_eq!(response.body(), &Body::Text("item saved".to_string()));
     }
+
+    #[tokio::test]
+    async fn test_put_item_when_dynamodb_fails_should_return_500() {
+        // Mock DynamoDB client
+        //
+        // `TestConnection` takes a vector of requests and responses, allowing us to
+        // simulate the behaviour of the DynamoDB API endpoint. Since we are only
+        // making a single request in this test, we only need to provide a single
+        // entry in the vector.
+        let conn = TestConnection::new(vec![(
+            get_request_builder()
+                .header("x-amz-target", "DynamoDB_20120810.PutItem")
+                .body(SdkBody::from(
+                    r#"{"TableName":"test","Item":{"id":{"S":"1"},"payload":{"S":"test1"}}}"#,
+                ))
+                .unwrap(),
+            http::Response::builder()
+                .status(500)
+                .body(SdkBody::from(
+                    r#"{"Attributes": {"id": {"S": "1"}, "payload": {"S": "test1"}}}"#,
+                ))
+                .unwrap(),
+        )]);
+        let client = Client::from_conf(get_mock_config(&conn).await);
+
+        let table_name = "test_table";
+
+        // Mock API Gateway request
+        let mut path_parameters = HashMap::new();
+        path_parameters.insert("id".to_string(), vec!["1".to_string()]);
+
+        let request = http::Request::builder()
+            .method("PUT")
+            .uri("/1")
+            .body(Body::Text("test1".to_string()))
+            .unwrap()
+            .with_path_parameters(path_parameters);
+
+        // Send mock request to Lambda handler function
+        let response = put_item(&client, table_name, request).await.unwrap();
+
+        // Assert that the response is correct
+        assert_eq!(response.status(), 500);
+        assert_eq!(response.body(), &Body::Text("internal error".to_string()));
+    }
 }
