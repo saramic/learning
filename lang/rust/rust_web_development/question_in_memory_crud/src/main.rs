816f0b4c84986486593035ec07c59e7feafece9c
Michael Milewski <saramic@gmail.com>
2024-05-30 08:48:42 +1000

    Add a simple warp filter and serialize json
    can:
      cargo run
      # and
      curl http://127.0.0.1:1337/questions
      {
        "id":"1","title":"Frist Question",
        "content":"Content of question","tags":["faq"]
      }

--- lang/rust/rust_web_development/question_in_memory_crud/src/main.rs
+++ lang/rust/rust_web_development/question_in_memory_crud/src/main.rs
@@ -1,7 +1,9 @@
+use warp::Filter;
+use serde::Serialize;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Question {
     id: QuestionId,
     title: String,
@@ -9,7 +11,7 @@ struct Question {
     tags: Option<Vec<String>>
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct QuestionId(String);
 
 impl FromStr for QuestionId {
@@ -41,12 +43,29 @@ impl Question {
     }
 }
 
-fn main() {
+async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
         QuestionId::from_str("1").expect("No id provided"),
         "Frist Question".to_string(),
         "Content of question".to_string(),
         Some(vec!("faq".to_string())),
     );
-    println!("{:?}", question);
+
+    Ok(warp::reply::json(
+            &question
+    ))
+}
+
+#[tokio::main]
+async fn main() {
+    let get_items = warp::get()
+        .and(warp::path("questions"))
+        .and(warp::path::end())
+        .and_then(get_questions);
+
+    let routes = get_items;
+
+    warp::serve(routes)
+        .run(([127, 0, 0, 1], 1337))
+        .await;
 }
