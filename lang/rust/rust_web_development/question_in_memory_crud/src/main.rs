e01122e644b0b0859f74faa81b274272c2ea0760
Michael Milewski <saramic@gmail.com>
2024-05-29 23:31:21 +1000

    Use FromStr trait for QuestionId

--- lang/rust/rust_web_development/question_in_memory_crud/src/main.rs
+++ lang/rust/rust_web_development/question_in_memory_crud/src/main.rs
@@ -1,3 +1,6 @@
+use std::io::{Error, ErrorKind};
+use std::str::FromStr;
+
 #[derive(Debug)]
 struct Question {
     id: QuestionId,
@@ -8,6 +11,19 @@ struct Question {
 
 #[derive(Debug)]
 struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(
+                Error::new(ErrorKind::InvalidInput, "No id provided")
+            ),
+        }
+    }
+}
 
 impl Question {
     fn new (
@@ -27,7 +43,7 @@ impl Question {
 
 fn main() {
     let question = Question::new(
-        QuestionId("1".to_string()),
+        QuestionId::from_str("1").expect("No id provided"),
         "Frist Question".to_string(),
         "Content of question".to_string(),
         Some(vec!("faq".to_string())),
