c3d9fb92c6d59b0a6b5ca17554bb8672b89ee777
Michael Milewski <saramic@gmail.com>
2024-05-29 23:14:59 +1000

    Simple Question struct
    using either `.to_string()` or `String::from(...)` to convert &str to
    String (which is mutable), Also using the Debug trait and {:#?} to print
    out arbitrary object

---
+++ lang/rust/rust_web_development/question_in_memory_crud/src/main.rs
@@ -0,0 +1,36 @@
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl Question {
+    fn new (
+        id: QuestionId,
+        title: String,
+        content: String,
+        tags: Option<Vec<String>>
+    ) -> Self {
+        Question {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
+fn main() {
+    let question = Question::new(
+        QuestionId("1".to_string()),
+        "Frist Question".to_string(),
+        "Content of question".to_string(),
+        Some(vec!("faq".to_string())),
+    );
+    println!("{:#?}", question);
+}
