aad216f6dde0a6e97d6369d919128cc3525b0e3b
Michael Milewski <saramic@gmail.com>
2024-05-30 09:08:39 +1000

    Add an error handler for route not found
    with:
      cargo run
    
      curl localhost:1337
      Route not found%
    
      curl localhost:1337/questions
      {"id":"1","title":"Frist Question" ...
    
      # presumably this should be the UNPROCESSABLE_ENTITY?
      curl localhost:1337/questions/2
      Route not found

--- lang/rust/rust_web_development/question_in_memory_crud/src/main.rs
+++ lang/rust/rust_web_development/question_in_memory_crud/src/main.rs
@@ -1,4 +1,4 @@
-use warp::Filter;
+use warp::{Filter, reject::Reject, Rejection, Reply, http::StatusCode};
 use serde::Serialize;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
@@ -43,6 +43,10 @@ impl Question {
     }
 }
 
+#[derive(Debug)]
+struct InvalidId;
+impl Reject for InvalidId {}
+
 async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
     let question = Question::new(
         QuestionId::from_str("1").expect("No id provided"),
@@ -51,9 +55,30 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
         Some(vec!("faq".to_string())),
     );
 
-    Ok(warp::reply::json(
-            &question
-    ))
+    match question.id.0.parse::<i32>() {
+        Err(_) => {
+            Err(warp::reject::custom(InvalidId))
+        },
+        Ok(_) => {
+            Ok(warp::reply::json(
+                &question
+            ))
+        }
+    }
+}
+
+async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
+    if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+                "No valid ID presented",
+                StatusCode::UNPROCESSABLE_ENTITY,
+        ))
+    } else {
+        Ok(warp::reply::with_status(
+                "Route not found",
+                StatusCode::NOT_FOUND,
+        ))
+    }
 }
 
 #[tokio::main]
@@ -61,7 +86,8 @@ async fn main() {
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
-        .and_then(get_questions);
+        .and_then(get_questions)
+        .recover(return_error);
 
     let routes = get_items;
 
