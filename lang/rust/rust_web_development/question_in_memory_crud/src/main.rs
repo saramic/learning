46ba6553698eb17ee0a66292fa198e6945b0b9ef
Michael Milewski <saramic@gmail.com>
2024-05-30 10:05:28 +1000

    Attempt to deal with CORS
    not really sure why the filters::cors::CorsForbidden was necessary as it
    seems the output was the same from curl in both cases
    
    curl -X OPTIONS localhost:1337/questions \
      -H "Access-Control-Request-Method: PUT" \
      -H "Access-Control-Request-Headers: content-type" \
      -H "Origin: https://not-origin.io" --verbose
    
        * Connected to localhost (127.0.0.1) port 1337
        > OPTIONS /questions HTTP/1.1
        > Access-Control-Request-Method: PUT
        > Access-Control-Request-Headers: content-type
        > Origin: https://not-origin.io
        >
        < HTTP/1.1 200 OK
        < access-control-allow-headers: content-type
        < access-control-allow-methods: PUT, POST, GET, DELETE
        < access-control-allow-origin: https://not-origin.io
    
    with:
      -H "Access-Control-Request-Headers: content-type-not" \
    
        * Connected to localhost (127.0.0.1) port 1337
        > OPTIONS /questions HTTP/1.1
        > Access-Control-Request-Method: PUT
        > Access-Control-Request-Headers: content-type-not
        > Origin: https://not-origin.io
        >
        < HTTP/1.1 403 Forbidden
    
        CORS request forbidden: header not allowed%

--- lang/rust/rust_web_development/question_in_memory_crud/src/main.rs
+++ lang/rust/rust_web_development/question_in_memory_crud/src/main.rs
@@ -1,7 +1,10 @@
 use serde::Serialize;
 use std::io::{Error, ErrorKind};
 use std::str::FromStr;
-use warp::{http::StatusCode, reject::Reject, Filter, Rejection, Reply};
+use warp::{
+    filters::cors::CorsForbidden, http::Method, http::StatusCode, reject::Reject, Filter,
+    Rejection, Reply,
+};
 
 #[derive(Debug, Serialize)]
 struct Question {
@@ -55,14 +58,20 @@ async fn get_questions() -> Result<impl warp::Reply, warp::Rejection> {
 }
 
 async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
-    if let Some(InvalidId) = r.find() {
+    // println!("{:?}", r);
+    if let Some(error) = r.find::<CorsForbidden>() {
         Ok(warp::reply::with_status(
-            "No valid ID presented",
+            error.to_string(),
+            StatusCode::FORBIDDEN,
+        ))
+    } else if let Some(InvalidId) = r.find() {
+        Ok(warp::reply::with_status(
+            "No valid ID presented".to_string(),
             StatusCode::UNPROCESSABLE_ENTITY,
         ))
     } else {
         Ok(warp::reply::with_status(
-            "Route not found",
+            "Route not found".to_string(),
             StatusCode::NOT_FOUND,
         ))
     }
@@ -70,13 +79,19 @@ async fn return_error(r: Rejection) -> Result<impl Reply, Rejection> {
 
 #[tokio::main]
 async fn main() {
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_header("content-type")
+        // .allow_header("not-in-the-request")
+        .allow_methods(&[Method::PUT, Method::DELETE, Method::GET, Method::POST]);
+
     let get_items = warp::get()
         .and(warp::path("questions"))
         .and(warp::path::end())
         .and_then(get_questions)
         .recover(return_error);
 
-    let routes = get_items;
+    let routes = get_items.with(cors);
 
     warp::serve(routes).run(([127, 0, 0, 1], 1337)).await;
 }
