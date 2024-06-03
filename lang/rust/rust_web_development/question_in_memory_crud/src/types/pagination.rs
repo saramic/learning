d41659fab73921727c3b3b62fdee2890f45d9c32
Michael Milewski <saramic@gmail.com>
2024-06-04 08:01:08 +1000

    Add some documentation
    with:
       /// /**...*/ being visible in docs
       //! /*!...*/ visible and apply to line above
       // /*...*/ not being visible
    
       publish with:
       cargo doc --open
    
       or take a look in:
         target/doc/question_in_memory_crud/index.html
       and the docs generated:
         target/doc/question_in_memory_crud/types/pagination/fn.extract_pagination.html

--- lang/rust/rust_web_development/question_in_memory_crud/src/types/pagination.rs
+++ lang/rust/rust_web_development/question_in_memory_crud/src/types/pagination.rs
@@ -1,19 +1,30 @@
 use handle_errors::Error;
 use std::collections::HashMap;
 
+/// Pagination struct that is getting extracted from query params
 #[derive(Debug)]
 pub struct Pagination {
+    /// The indes of the first item that has to be returned
     pub start: usize,
+    /// The index of the last itme that has to be returned
     pub end: usize,
 }
 
+/// Extract query parameters from the `/questions` route
+/// # Example query
+/// GET requests to this route can have a paginatino attached so we just return the questions we
+/// need
+/// `/questions?start=1&end=10`
 pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
+    // Could be improved in the future
     if params.contains_key("start") && params.contains_key("end") {
+        // Takes the "start" parameter in the query and tries to convert it to a number
         let start = params
             .get("start")
             .unwrap()
             .parse::<usize>()
             .map_err(Error::ParseError)?;
+        // Takes the "end" parameter in the query and tries to convert it to a number
         let end = params
             .get("end")
             .unwrap()
