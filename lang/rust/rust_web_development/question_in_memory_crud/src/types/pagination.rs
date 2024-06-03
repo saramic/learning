c59f62742cb0edfce70caf82e53fd0aae17fb921
Michael Milewski <saramic@gmail.com>
2024-06-04 08:08:38 +1000

    Code example in comments
    supposedly these will execute as part of tests if in a library, ie
    `cargo new --lib`

--- lang/rust/rust_web_development/question_in_memory_crud/src/types/pagination.rs
+++ lang/rust/rust_web_development/question_in_memory_crud/src/types/pagination.rs
@@ -15,6 +15,15 @@ pub struct Pagination {
 /// GET requests to this route can have a paginatino attached so we just return the questions we
 /// need
 /// `/questions?start=1&end=10`
+/// # Example usage
+/// ```rust
+/// let mut query = HasMap::new();
+/// query.insert("start".to_string(), "1".to_string());
+/// query.insert("end".to_string(), "1".to_string());
+/// let p = types::pagination::extract_pagination(query).unwrap();
+/// assert_eq!(p.start, 1);
+/// assert_eq!(p.end, 10);
+/// ```
 pub fn extract_pagination(params: HashMap<String, String>) -> Result<Pagination, Error> {
     // Could be improved in the future
     if params.contains_key("start") && params.contains_key("end") {
