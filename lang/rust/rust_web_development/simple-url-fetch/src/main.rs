ac396db230444985e6d9c03936eefdc08fab7deb
Michael Milewski <saramic@gmail.com>
2024-06-10 22:48:05 +1000

    Updated to newest version of hyper
    This was very confusing
    - FINALLY
      https://github.com/hyperium/hyper-util/blob/master/examples/client.rs
      got this working by using
      ```
      hyper_util::client::legacy::Client
      ```
      although this was no silver bullet as some other stuff changed
    
    INITIALLY tried a bunch of broken code from docs and Github
    
    - https://docs.rs/hyper/latest/hyper/client/conn/index.html
      suggested
      ```
      let (mut request_sender, connection) = conn::http1::handshake(tcp).await?;
      ```
      but what is `tcp`? this code does not run
    - https://github.com/hyperium/hyper/blob/master/examples/client.rs
      code example suggests
      ```
      let io = TokioIo::new(stream);
    
      let (mut sender, conn) = hyper::client::conn::http1::handshake(io).await?;
      ```
      but Tokio seems to come from
      https://github.com/hyperium/hyper/blob/master/benches/support/tokiort.rs
      and a bunch of other stuff?

--- lang/rust/rust_web_development/simple-url-fetch/src/main.rs
+++ lang/rust/rust_web_development/simple-url-fetch/src/main.rs
@@ -1,5 +1,8 @@
 use std::env;
-use hyper::{body::HttpBody as _, Client};
+
+use http_body_util::{BodyExt, Empty};
+use hyper::Request;
+use hyper_util::client::legacy::{connect::HttpConnector, Client};
 use tokio::io::{self, AsyncWriteExt as _};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
@@ -14,18 +17,22 @@ async fn main() -> Result<()> {
         }
     };
 
-    let client = Client::new();
+    let client = Client::builder(hyper_util::rt::TokioExecutor::new()).build(HttpConnector::new());
 
-    let mut res = client
-        .get(url.parse::<hyper::Uri>().unwrap())
-        .await?;
+    let req = Request::builder()
+        .uri(url.parse::<hyper::Uri>().unwrap())
+        .body(Empty::<bytes::Bytes>::new())?;
+
+    let mut res = client.request(req).await?;
 
     println!("Response: {}", res.status());
     println!("Headers: {:?}\n", res.headers());
 
-    while let Some(next) = res.data().await {
-        let chunk = next?;
-        io::stdout().write_all(&chunk).await?;
+    while let Some(next) = res.frame().await {
+        let frame = next?;
+        if let Some(chunk) = frame.data_ref() {
+            io::stdout().write_all(&chunk).await?;
+        }
     }
 
     println!("\n\nDone!");
