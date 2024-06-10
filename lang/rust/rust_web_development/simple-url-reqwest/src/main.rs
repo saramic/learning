6405afaf6907a00716607dd514bbe1c038acc628
Michael Milewski <saramic@gmail.com>
2024-06-10 23:47:56 +1000

    Switch api call to apilayer bad_words filter

--- lang/rust/rust_web_development/simple-url-reqwest/src/main.rs
+++ lang/rust/rust_web_development/simple-url-reqwest/src/main.rs
@@ -1,9 +1,18 @@
+use std::env;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let api_layer_api_key = env::var("API_LAYER_API_KEY")
+        .unwrap_or_else(|_| "missing ApiLayer API_LAYER_API_KEY".to_string());
+
+    let mut body_text: Vec<String> = env::args().collect();
+    body_text.remove(0);
+
     let client = reqwest::Client::new();
     let res = client
-        .post("http://httpbin.org/post")
-        .body("the exact body is sent")
+        .post("https://api.apilayer.com/bad_words?censor_character=*")
+        .header("apikey", api_layer_api_key)
+        .body(body_text.join(" "))
         .send()
         .await?
         .text()
