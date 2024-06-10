efa504e742560d11bf746cfdd734c9ed66aafb75
Michael Milewski <saramic@gmail.com>
2024-06-10 17:55:37 +1000

    Feature addition of passing url from command line

--- lang/rust/rust_web_development/simple-url-fetch/src/main.rs
+++ lang/rust/rust_web_development/simple-url-fetch/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use hyper::{body::HttpBody as _, Client};
 use tokio::io::{self, AsyncWriteExt as _};
 
@@ -5,10 +6,18 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let url = match env::args().nth(1) {
+        Some(url) => url,
+        None => {
+            println!("Usage: client <url>");
+            return Ok(());
+        }
+    };
+
     let client = Client::new();
 
     let mut res = client
-        .get("http://www.google.com".parse::<hyper::Uri>().unwrap())
+        .get(url.parse::<hyper::Uri>().unwrap())
         .await?;
 
     println!("Response: {}", res.status());
