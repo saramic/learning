aee290c9037ed0a043c730da0b8262010f383323
Michael Milewski <saramic@gmail.com>
2024-06-17 06:35:19 +1000

    Use Argon2 recommended password hashing algorithm
    as recommended by:
        OWASP password storage cheat sheet
        https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
        the spec
        https://github.com/P-H-C/phc-winner-argon2/blob/master/argon2-specs.pdf
    
    now:
        echo '{
            "email":"email'`date +%S`'@example.com",
            "password":"clear_text_password"
        }' | curl --json @- localhost:1337/registration
    
        Account added
    
    and in the DB:
        psql rustwebdev -XtAc "SELECT * FROM accounts"
        1|email48@example.com| \
            $argon2id$v=19$m=19456,t=2,p=1$tdhbCmDig/tgKZH/evieLv6l9lHaSxm \
            wAWGbAv8FGJY$DQwK+Tj5dyoWrLcT4fO0Quji3VmK49CmjIc5LSC9+ng

--- lang/rust/rust_web_development/question-bad-words/src/routes/authentication.rs
+++ lang/rust/rust_web_development/question-bad-words/src/routes/authentication.rs
@@ -1,3 +1,5 @@
+use argon2::{self, Config};
+use rand::Rng;
 use warp::http::StatusCode;
 
 use crate::store::Store;
@@ -7,8 +9,22 @@ pub async fn register(
     store: Store,
     account: Account,
 ) -> Result<impl warp::Reply, warp::Rejection> {
+    let hashed_password = hash_password(account.password.as_bytes());
+
+    let account = Account {
+        id: account.id,
+        email: account.email,
+        password: hashed_password,
+    };
+
     match store.add_account(account).await {
         Ok(_) => Ok(warp::reply::with_status("Account added", StatusCode::OK)),
         Err(e) => Err(warp::reject::custom(e)),
     }
+}
+
+pub fn hash_password(password: &[u8]) -> String {
+    let salt = rand::thread_rng().gen::<[u8; 32]>();
+    let config = Config::default();
+    argon2::hash_encoded(password, &salt, &config).unwrap()
 }
