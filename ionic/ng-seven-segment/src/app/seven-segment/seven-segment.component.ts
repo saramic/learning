7edb3d0806d0ddb18607f577adcff90b395817a3
Michael Milewski <saramic@gmail.com>
2024-07-02 20:03:35 +1000

    [IONIC] naive numbers in seven segment

--- ionic/ng-seven-segment/src/app/seven-segment/seven-segment.component.ts
+++ ionic/ng-seven-segment/src/app/seven-segment/seven-segment.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-seven-segment',
@@ -7,8 +8,8 @@ import { Component } from '@angular/core';
   templateUrl: './seven-segment.component.html',
 })
 export class SevenSegmentComponent {
-  onColor = "#FF0000";
-  offColor = "#F5E1E1";
+  onColor = '#FF0000';
+  offColor = '#F5E1E1';
   colorA = this.offColor;
   colorB = this.offColor;
   colorC = this.offColor;
@@ -16,8 +17,115 @@ export class SevenSegmentComponent {
   colorE = this.offColor;
   colorF = this.offColor;
   colorG = this.offColor;
+  current = 0;
+
+  source = timer(0, 300);
 
   ngOnInit() {
-    console.log('SevenSegmentComponent initialized');
+    this.source.subscribe(() => this.next());
+  }
+
+  next() {
+    this.current = (this.current + 1) % 10;
+    switch (this.current) {
+      case 0:
+        this.colorA = this.onColor;
+        this.colorB = this.onColor;
+        this.colorC = this.onColor;
+        this.colorD = this.onColor;
+        this.colorE = this.onColor;
+        this.colorF = this.onColor;
+        this.colorG = this.offColor;
+        break;
+      case 1:
+        this.colorA = this.offColor;
+        this.colorB = this.onColor;
+        this.colorC = this.onColor;
+        this.colorD = this.offColor;
+        this.colorE = this.offColor;
+        this.colorF = this.offColor;
+        this.colorG = this.offColor;
+        break;
+      case 2:
+        this.colorA = this.onColor;
+        this.colorB = this.onColor;
+        this.colorC = this.offColor;
+        this.colorD = this.onColor;
+        this.colorE = this.onColor;
+        this.colorF = this.offColor;
+        this.colorG = this.onColor;
+        break;
+      case 3:
+        this.colorA = this.onColor;
+        this.colorB = this.onColor;
+        this.colorC = this.onColor;
+        this.colorD = this.onColor;
+        this.colorE = this.offColor;
+        this.colorF = this.offColor;
+        this.colorG = this.onColor;
+        break;
+      case 4:
+        this.colorA = this.offColor;
+        this.colorB = this.onColor;
+        this.colorC = this.onColor;
+        this.colorD = this.offColor;
+        this.colorE = this.offColor;
+        this.colorF = this.onColor;
+        this.colorG = this.onColor;
+        break;
+      case 5:
+        this.colorA = this.onColor;
+        this.colorB = this.offColor;
+        this.colorC = this.onColor;
+        this.colorD = this.onColor;
+        this.colorE = this.offColor;
+        this.colorF = this.onColor;
+        this.colorG = this.onColor;
+        break;
+      case 6:
+        this.colorA = this.onColor;
+        this.colorB = this.offColor;
+        this.colorC = this.onColor;
+        this.colorD = this.onColor;
+        this.colorE = this.onColor;
+        this.colorF = this.onColor;
+        this.colorG = this.onColor;
+        break;
+      case 7:
+        this.colorA = this.onColor;
+        this.colorB = this.onColor;
+        this.colorC = this.onColor;
+        this.colorD = this.offColor;
+        this.colorE = this.offColor;
+        this.colorF = this.offColor;
+        this.colorG = this.offColor;
+        break;
+      case 8:
+        this.colorA = this.onColor;
+        this.colorB = this.onColor;
+        this.colorC = this.onColor;
+        this.colorD = this.onColor;
+        this.colorE = this.onColor;
+        this.colorF = this.onColor;
+        this.colorG = this.onColor;
+        break;
+      case 9:
+        this.colorA = this.onColor;
+        this.colorB = this.onColor;
+        this.colorC = this.onColor;
+        this.colorD = this.onColor;
+        this.colorE = this.offColor;
+        this.colorF = this.onColor;
+        this.colorG = this.onColor;
+        break;
+      default:
+        this.colorA = this.offColor;
+        this.colorB = this.offColor;
+        this.colorC = this.offColor;
+        this.colorD = this.offColor;
+        this.colorE = this.offColor;
+        this.colorF = this.offColor;
+        this.colorG = this.offColor;
+    }
   }
 }
