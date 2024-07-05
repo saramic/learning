41f96c1015136a68c36caa26bc33a0d659fbc6be
Michael Milewski <saramic@gmail.com>
2024-07-06 08:16:23 +1000

    [IONIC] switch routes
    from
        http://localhost:8100/recipes/recipe-detail
    to
        http://localhost:8100/recipes/id1
        http://localhost:8100/recipes/id2

--- ionic/ionic-angular-course/src/app/app-routing.module.ts
+++ ionic/ionic-angular-course/src/app/app-routing.module.ts
@@ -9,7 +9,16 @@ const routes: Routes = [
   },
   {
     path: 'recipes',
-    loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule)
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./recipes/recipes.module').then( m => m.RecipesPageModule),
+      },
+      {
+        path: ':recipeId',
+        loadChildren: () => import('./recipes/recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule),
+      }
+    ]
   },
 ];
 
--- ionic/ionic-angular-course/src/app/recipes/recipes-routing.module.ts
+++ ionic/ionic-angular-course/src/app/recipes/recipes-routing.module.ts
@@ -7,10 +7,6 @@ const routes: Routes = [
   {
     path: '',
     component: RecipesPage
-  },
-  {
-    path: 'recipe-detail',
-    loadChildren: () => import('./recipe-detail/recipe-detail.module').then( m => m.RecipeDetailPageModule)
   }
 ];
 
