9589dd744598496f68f8452786e67eae1bd0827a
Michael Milewski <saramic@gmail.com>
2024-07-06 18:08:45 +1000

    [IONIC] setup android and ios app environment
    using:
        # installed some android studio tools
    
        ng build # needed to build your project
        ionic capacitor add android
        ionic capacitor copy android
        ionic capacitor run android
        # didn't seem to connect to reload server
        ionic capacitor run android --livereload
    
        # install Xcode via App Store
        ionic capacitor add ios
        ionic capacitor copy ios
        ionic capacitor run ios -l # livereload

---
+++ ionic/ionic-angular-course/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'saramic.ionic.recipe',
+  appName: 'ionic-angular-course',
+  webDir: 'www'
+};
+
+export default config;
