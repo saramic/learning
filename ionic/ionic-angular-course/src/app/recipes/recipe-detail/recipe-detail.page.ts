dbf1904b625d1704b90e82434c9f4f786b75e848
Michael Milewski <saramic@gmail.com>
2024-07-06 15:15:01 +1000

    [IONIC] delete a recipe
    - added ability to deleteRecipe
    - also using AlertController for a modal delete/cancel
    - using this.router.navigate(['/recipes']); to navigate
    
    there are a couple of issues
    - if starting on /recipies going to a recipie and deleting will not
      work, but will work if you start on a recipe and delete, one will be
      deleted - later on will cover how to make this work
    - an invalid ID does not redirect at this stage, just no id

--- ionic/ionic-angular-course/src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ ionic/ionic-angular-course/src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RecipesService } from '../recipes.service';
 import { Recipe } from '../recipe.model';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -13,18 +15,46 @@ export class RecipeDetailPage implements OnInit {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private recipesService: RecipesService
+    private recipesService: RecipesService,
+    private router: Router,
+    private alertCtrl: AlertController
   ) {}
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe((paramMap) => {
       if (!paramMap.has('recipeId')) {
-        // redirect
+        // redirect if no ID is passed in
+        // TODO: should really also be done below if it cannot be found by
+        //        getRecipe() method
+        this.router.navigate(['/recipes']);
         return;
       }
       const recipeId = paramMap.get('recipeId')!;
       // console.log(recipeId);
       this.loadedRecipe = this.recipesService.getRecipe(recipeId);
+    });
+  }
+
+  onDeleteRecipie() {
+    this.alertCtrl.create({
+      header: 'Are you sure?',
+      message: 'Do you really want to delete the recipe?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            this.recipesService.deleteRecipe(this.loadedRecipe.id);
+            this.router.navigate(['/recipes']);
+          },
+        },
+      ],
+    })
+    .then((alertEl) => {
+      alertEl.present();
     });
   }
 }
--- ionic/ionic-angular-course/src/app/recipes/recipes.service.ts
+++ ionic/ionic-angular-course/src/app/recipes/recipes.service.ts
@@ -31,4 +31,8 @@ export class RecipesService {
   getRecipe(recipeId: string) {
     return { ...this.recipes.find((recipe) => recipe.id === recipeId) } as Recipe;
   }
+
+  deleteRecipe(recipeId: string) {
+    this.recipes = this.recipes.filter((recipe) => recipe.id !== recipeId);
+  }
 }
