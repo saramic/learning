97cab54e57a625414d3e79a8ecb9685cdd00d312
Michael Milewski <saramic@gmail.com>
2024-07-12 08:21:01 +1000

    [RXJS] add complete and teardown callbacks

--- rxjs/examples/src/index.ts
+++ rxjs/examples/src/index.ts
@@ -12,12 +12,25 @@ const observable$ = new Observable<string>(subscriber => {
   output('Observable executed');
   subscriber.next('Alice');
   subscriber.next('Ben');
-  setTimeout(() => subscriber.next('Charlie'), 2000);
+  setTimeout(() => {
+    subscriber.next('Charlie');
+    subscriber.complete();
+  }, 2000);
+
+  return () => {
+    // allow to teardown for cleanup or cancelation
+    output("Teardown");
+  };
 })
 
 output('Before subscribe');
 output('Immedaite synchronous output of Alice and Ben');
-observable$.subscribe(value => output(value));
+observable$.subscribe({
+  next: value => output(value),
+  complete: () => output("Complete"),
+});
 output('After subscribe');
 output('2 seconds later an asynchronous output of Charlie');
+output('... followed by the complete hook');
+output('... and then the Teardown hook');
 
