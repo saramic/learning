818e37d1cb58129de68e106a6e3819cc7754c465
Michael Milewski <saramic@gmail.com>
2024-07-12 08:27:29 +1000

    [RXJS] handling subscriber errors

--- rxjs/examples/src/index.ts
+++ rxjs/examples/src/index.ts
@@ -14,8 +14,13 @@ const observable$ = new Observable<string>(subscriber => {
   subscriber.next('Ben');
   setTimeout(() => {
     subscriber.next('Charlie');
-    subscriber.complete();
+    // remove complete to deal with error 4s after
+    // subscriber.complete();
   }, 2000);
+  setTimeout(() => 
+    subscriber.error(new Error("error")),
+    4000
+  );
 
   return () => {
     // allow to teardown for cleanup or cancelation
@@ -27,10 +32,12 @@ output('Before subscribe');
 output('Immedaite synchronous output of Alice and Ben');
 observable$.subscribe({
   next: value => output(value),
-  complete: () => output("Complete"),
+  complete: () => output('Complete'),
+  error: error => output(error),
 });
 output('After subscribe');
 output('2 seconds later an asynchronous output of Charlie');
-output('... followed by the complete hook');
+// output('... followed by the complete hook');
+output('4 seconds later the error');
 output('... and then the Teardown hook');
 
