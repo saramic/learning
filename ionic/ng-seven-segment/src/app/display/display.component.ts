9e14cf94083f4ff976f1d84796e8609247b7a0d6
Michael Milewski <saramic@gmail.com>
2024-07-13 23:00:49 +1000

    [IONIC] move control into display
    allowing a 100ms tick to control any number of 7 segment display
    components to display loop, a timer and a clock

--- ionic/ng-seven-segment/src/app/display/display.component.ts
+++ ionic/ng-seven-segment/src/app/display/display.component.ts
@@ -1,17 +1,47 @@
 import { Component } from '@angular/core';
 import { SevenSegmentComponent } from '../seven-segment/seven-segment.component';
 import { Router, RouterModule } from '@angular/router';
+import { timer } from 'rxjs';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-display',
   standalone: true,
-  imports: [SevenSegmentComponent, RouterModule],
+  imports: [SevenSegmentComponent, RouterModule, CommonModule],
   templateUrl: './display.component.html',
 })
 export class DisplayComponent {
+  last = new Date().getTime();
+  loopBitStates = ['80', '40', '20', '10', '8', '4'];
+  numberBitStates = [
+    'fc', // parseInt('11111100', 2).toString(16),
+    '60', // parseInt('01100000', 2).toString(16),
+    'da', // parseInt('11011010', 2).toString(16),
+    'f2', // parseInt('11110010', 2).toString(16),
+    '66', // parseInt('01100110', 2).toString(16),
+    'b6', // parseInt('10110110', 2).toString(16),
+    'be', // parseInt('10111110', 2).toString(16),
+    'e0', // parseInt('11100000', 2).toString(16),
+    'fe', // parseInt('11111110', 2).toString(16),
+    'f6', // parseInt('11110110', 2).toString(16),
+    'fa', // parseInt('11111010', 2).toString(16), // a
+    '3e', // parseInt('00111110', 2).toString(16), // b
+    '1a', // parseInt('00011010', 2).toString(16), // c
+    '7a', // parseInt('01111010', 2).toString(16), // d
+    '9e', // parseInt('10011110', 2).toString(16), // e
+    '8e', // parseInt('10001110', 2).toString(16), // f
+  ];
+  current = 0;
   title = 'ng-seven-segment';
   type = 'loop';
+  segments = ['fc', 'fc', 'fc', 'fc'];
+
+  source = timer(0, 100);
+
   constructor(private router: Router) {}
+
   ngOnInit() {
+    this.source.subscribe(() => this.tick());
+
     switch (this.router.url) {
       case '/clock':
         this.title = 'Clock';
@@ -28,5 +58,58 @@ export class DisplayComponent {
       default:
         this.title = 'ng-seven-segment';
     }
+  }
+
+  tick() {
+    switch (this.type) {
+      case 'clock':
+        this.clock();
+        break;
+      case 'timer':
+        const now = new Date().getTime();
+        if (now > this.last + 100) {
+          this.next();
+          this.last = now;
+        }
+        break;
+      default:
+        this.loop();
+    }
+  }
+
+  clock() {
+    const date = new Date();
+    const seconds = date.getSeconds().toString().padStart(2, '0').split("");
+    const minutes = date.getMinutes().toString().padStart(2, '0').split("");
+    const hours = date.getHours().toString().padStart(2, '0').split("");
+    this.segments = [
+      this.numberBitStates[parseInt(hours[0])],
+      this.numberBitStates[parseInt(hours[1])],
+      this.numberBitStates[parseInt(minutes[0])],
+      this.numberBitStates[parseInt(minutes[1])],
+      this.numberBitStates[parseInt(seconds[0])],
+      this.numberBitStates[parseInt(seconds[1])],
+    ]
+  }
+
+  loop() {
+    this.current = (this.current + 1) % this.loopBitStates.length;
+    this.segments = [
+      this.loopBitStates[this.current],
+      this.loopBitStates[this.current],
+      this.loopBitStates[this.current],
+      this.loopBitStates[this.current],
+    ];
+  }
+
+  next() {
+    this.current = (this.current + 1) % Math.pow(10, this.segments.length);
+    this.current
+      .toString()
+      .padStart(this.segments.length, '0')
+      .split('')
+      .forEach((digit, index) => {
+        this.segments[index] = this.numberBitStates[parseInt(digit)];
+      });
   }
 }
--- ionic/ng-seven-segment/src/app/seven-segment/seven-segment.component.ts
+++ ionic/ng-seven-segment/src/app/seven-segment/seven-segment.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from '@angular/core';
-import { timer } from 'rxjs';
+import { Component, Input, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-seven-segment',
@@ -8,8 +7,7 @@ import { timer } from 'rxjs';
   templateUrl: './seven-segment.component.html',
 })
 export class SevenSegmentComponent {
-  @Input() type: String;
-  last = new Date().getTime();
+  @Input() bitState: string = "fc";
 
   onColor = '#FF0000';
   offColor = '#F5E1E1';
@@ -21,67 +19,13 @@ export class SevenSegmentComponent {
   colorF = this.offColor;
   colorG = this.offColor;
 
-  loopBitStates = ['80', '40', '20', '10', '8', '4'];
-  numberBitStates = [
-    'fc', // parseInt('11111100', 2).toString(16),
-    '60', // parseInt('01100000', 2).toString(16),
-    'da', // parseInt('11011010', 2).toString(16),
-    'f2', // parseInt('11110010', 2).toString(16),
-    '66', // parseInt('01100110', 2).toString(16),
-    'b6', // parseInt('10110110', 2).toString(16),
-    'be', // parseInt('10111110', 2).toString(16),
-    'e0', // parseInt('11100000', 2).toString(16),
-    'fe', // parseInt('11111110', 2).toString(16),
-    'f6', // parseInt('11110110', 2).toString(16),
-    'fa', // parseInt('11111010', 2).toString(16), // a
-    '3e', // parseInt('00111110', 2).toString(16), // b
-    '1a', // parseInt('00011010', 2).toString(16), // c
-    '7a', // parseInt('01111010', 2).toString(16), // d
-    '9e', // parseInt('10011110', 2).toString(16), // e
-    '8e', // parseInt('10001110', 2).toString(16), // f
-  ];
+  constructor() { }
 
-  current = 0;
-
-  source = timer(0, 100);
-
-  constructor() {
-    this.type = 'timer';
-  }
-
-  ngOnInit() {
-    this.source.subscribe(() => this.tick());
-  }
-
-  tick() {
-    switch (this.type) {
-      case 'clock':
-        this.loop();
-        // const date = new Date();
-        // const seconds = date.getSeconds();
-        // const minutes = date.getMinutes();
-        // const hour = date.getHours();
-        break;
-      case 'timer':
-        const now = new Date().getTime();
-        if(now > this.last + 300) {
-          this.next();
-          this.last = now;
-        }
-        break;
-      default:
-        this.loop();
-    }
-  }
-
-  loop() {
-    this.current = (this.current + 1) % this.loopBitStates.length;
-    this.setSegments(this.loopBitStates[this.current]);
-  }
-
-  next() {
-    this.current = (this.current + 1) % 10;
-    this.setSegments(this.numberBitStates[this.current]);
+  ngOnChanges(changes: SimpleChanges) {
+    // console.log("Changes detected");
+    // console.log(changes['bitState'].currentValue)
+    this.bitState = changes['bitState'].currentValue;
+    this.setSegments(this.bitState);
   }
 
   setSegments(bitState: string) {
