5da56286ae1edd4567fca36b8ae776a4e2dc1a8a
Michael Milewski <saramic@gmail.com>
2024-07-14 09:59:17 +1000

    [IONIC] add a fin state for a box
    A final colour of who closed the box off - not sure about all the logic
    but a good initial go, there is probably some testing and refactoring
    that could be done to make this clear as to what is happening.

--- ionic/dots-and-boxes/src/app/game/board/board.component.ts
+++ ionic/dots-and-boxes/src/app/game/board/board.component.ts
@@ -26,6 +26,7 @@ export class BoardComponent implements OnInit {
         r: this.colorOff,
         b: this.colorOff,
         l: this.colorOff,
+        fin: null,
       };
       return cell;
     })
@@ -39,6 +40,60 @@ export class BoardComponent implements OnInit {
     // console.log('Cell clicked');
     // console.log(i, j, k);
     this.board[i][j][k] = this.turn ? this.colorOnOne : this.colorOnTwo;
+    this.checkBoxClosed(i, j, k);
+    if (k === 't' && i > 0) {
+      this.checkBoxClosed(i - 1, j, k);
+    }
+    if (k === 'l' && j > 0) {
+      this.checkBoxClosed(i, j - 1, k);
+    }
     this.turn = !this.turn;
+  }
+
+  checkBoxClosed(i: number, j: number, k: string) {
+    // non right bottom edge case
+    if (i < this.board.length - 1 && j < this.board[i].length - 1) {
+      if (
+        this.board[i][j]['t'] !== this.colorOff &&
+        this.board[i][j + 1]['l'] !== this.colorOff &&
+        this.board[i][j]['l'] !== this.colorOff &&
+        this.board[i + 1][j]['t'] !== this.colorOff
+      ) {
+        this.board[i][j]['fin'] = this.turn ? this.colorOnOne : this.colorOnTwo;
+      }
+    }
+    // right edge case
+    if (i < this.board.length - 1 && j === this.board[i].length - 1) {
+      if (
+        this.board[i][j]['t'] !== this.colorOff &&
+        this.board[i][j]['r'] !== this.colorOff &&
+        this.board[i][j]['l'] !== this.colorOff &&
+        this.board[i + 1][j]['t'] !== this.colorOff
+      ) {
+        this.board[i][j]['fin'] = this.turn ? this.colorOnOne : this.colorOnTwo;
+      }
+    }
+    // bottom edge case
+    if (i === this.board.length - 1 && j < this.board[i].length - 1) {
+      if (
+        this.board[i][j]['t'] !== this.colorOff &&
+        this.board[i][j + 1]['l'] !== this.colorOff &&
+        this.board[i][j]['l'] !== this.colorOff &&
+        this.board[i][j]['b'] !== this.colorOff
+      ) {
+        this.board[i][j]['fin'] = this.turn ? this.colorOnOne : this.colorOnTwo;
+      }
+    }
+    // bottom right
+    if (i === this.board.length - 1 && j === this.board[i].length - 1) {
+      if (
+        this.board[i][j]['t'] !== this.colorOff &&
+        this.board[i][j]['b'] !== this.colorOff &&
+        this.board[i][j]['l'] !== this.colorOff &&
+        this.board[i][j]['r'] !== this.colorOff
+      ) {
+        this.board[i][j]['fin'] = this.turn ? this.colorOnOne : this.colorOnTwo;
+      }
+    }
   }
 }
