bd2ca97d2d5cb0f74b47ba0d12ee249deef0c812
Michael Milewski <saramic@gmail.com>
2024-07-26 22:40:15 +1000

    [NG] Display users and images
    bringing in user avatars and a json of dummy users, display user name
    and image using standard getters and bindings

--- angular/ng-complete-guide-task-management/src/app/app.component.ts
+++ angular/ng-complete-guide-task-management/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'ng-complete-guide-task-management';
