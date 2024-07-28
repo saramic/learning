0d851bc39e778fa01092aef07f83d8b894f94364
Michael Milewski <saramic@gmail.com>
2024-07-28 13:33:54 +1000

    [NG] use typescript type or angular interface
    to define a complex object

--- angular/ng-complete-guide-task-management/src/app/user/user.component.ts
+++ angular/ng-complete-guide-task-management/src/app/user/user.component.ts
@@ -1,5 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+// NOTE: more Angular approach to a type above but can only be used for ojbects
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,11 +19,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) user!: {
-    id: string;
-    name: string;
-    avatar: string;
-  };
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
