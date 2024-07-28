fa1c9b3b3ef634646418fc9df5aef239753636b7
Michael Milewski <saramic@gmail.com>
2024-07-28 13:46:34 +1000

    [NG] *ngIf and *ngFor older structural directives
    used in older projects

--- angular/ANGULAR_COMPLETE_GUIDE_COURSE.md
+++ angular/ANGULAR_COMPLETE_GUIDE_COURSE.md
@@ -5,6 +5,14 @@
 - https://www.udemy.com/course/the-complete-guide-to-angular-2
 - https://github.com/mschwarzmueller/angular-complete-guide-course-resources
 
+## Sun 28 July
+- input/output
+- more on getters
+- typescript ! to guarantee a value will be returned, ok if using angular and
+  `required: true`
+- @for and @if for Angular 17 up
+- `*ngFor` and `*ngIf` structural directives for older projects
+
 ## Fri 26 July
 
 - general overview
--- angular/ng-complete-guide-task-management/src/app/app.component.ts
+++ angular/ng-complete-guide-task-management/src/app/app.component.ts
@@ -4,11 +4,19 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, UserComponent, TasksComponent],
+  imports: [
+    HeaderComponent,
+    NgFor,
+    NgIf,
+    RouterOutlet,
+    TasksComponent,
+    UserComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
