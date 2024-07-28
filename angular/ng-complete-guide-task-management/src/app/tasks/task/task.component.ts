7b0813ecdcf8fd689f9e030f544eda086d338fba
Michael Milewski <saramic@gmail.com>
2024-07-28 19:41:28 +1000

    [NG] use date pipe to format date
    DatePipe added using a `|` pipe and can have attributes passed as we do
    here with `date:'fullDate'`

--- angular/ng-complete-guide-task-management/src/app/tasks/task/task.component.ts
+++ angular/ng-complete-guide-task-management/src/app/tasks/task/task.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task } from './task.model';
 import { CardComponent } from "../../shared/card/card.component";
-
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
