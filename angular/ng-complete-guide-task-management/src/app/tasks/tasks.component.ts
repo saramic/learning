1d34a45a093f9038c31fecc7eb1aa0aef25e91fe
Michael Milewski <saramic@gmail.com>
2024-07-28 14:05:22 +1000

    [NG] filter out tasks by user id
    using another getter

--- angular/ng-complete-guide-task-management/src/app/tasks/tasks.component.ts
+++ angular/ng-complete-guide-task-management/src/app/tasks/tasks.component.ts
@@ -9,5 +9,26 @@ import { TaskComponent } from './task/task.component';
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string; // ? or could use string | undefined
+  @Input({required: true}) userId!: string;
+  @Input({required: true}) name!: string;
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Master Angular',
+      summary: 'Learn all the basics and advanced features',
+      dueDate: '2025-12-31',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title: 'Build first prototype',
+      summary: 'Learn all the basics and advanced features',
+      dueDate: '2025-12-31',
+    },
+  ]
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
