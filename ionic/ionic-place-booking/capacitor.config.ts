e457e9b38ccd7447f789d08df08166956db5515a
Michael Milewski <saramic@gmail.com>
2024-07-29 20:32:50 +1000

    [IONIC] new course work app for place booking
    with:
        ionic start ionic-place-booking
    
        ? Framework: Angular
        ? Starter template: blank
        Standalone

---
+++ ionic/ionic-place-booking/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-place-booking',
+  webDir: 'www'
+};
+
+export default config;
